a4929e816090950ee51407123f54b13df4f9f31b
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-12 20:41:33 -0300

    refactor : create basic style
    - created simple reset
    - created simple style

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -6,23 +6,24 @@ class App extends Component {
       <div className="container">
 
         <div className="search">
-          <input type="search" />
+          <input className="search__input" type="search" placeholder="Type a username to search"/>
         </div>
 
         <div className="user">
           <img className="user__photo" src="https://avatars3.githubusercontent.com/u/13966565?v=4" alt="Profile Samuel Silva" />
-          <h1 className="user__name">
-            <a href="https://github.com/samuelsilvadev">
-              Samuel Silva
+          <div className="user__details">
+            <h1 className="user__name">
+              <a href="https://github.com/samuelsilvadev">
+                Samuel Silva
             </a>
-          </h1>
+            </h1>
 
-          <ul>
-            <li>Repos: 100</li>
-            <li>Followers: 10</li>
-            <li>Following: 20</li>
-          </ul>
-
+            <ul className="user__counters">
+              <li>Repos: 100</li>
+              <li>Followers: 10</li>
+              <li>Following: 20</li>
+            </ul>
+          </div>
           <div className="user__actions">
             <button>See repositories</button>
             <button>See favorites</button>
@@ -31,13 +32,13 @@ class App extends Component {
         <div className="repositories">
           <h2>Repositories</h2>
           <ul>
-            <li>name</li>
+            <li><a href="#">name</a></li>            
           </ul>
         </div>
-        <div className="repositories">
+        <div className="starreds">
           <h2>Favorites</h2>
           <ul>
-            <li>name</li>
+            <li><a href="#">name</a></li>
           </ul>
         </div>
       </div>
