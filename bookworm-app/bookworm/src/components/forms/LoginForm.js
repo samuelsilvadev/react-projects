e9006d4869d58f0a9c76ea49192ed3a7acc8219b
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-19 13:29:38 -0300

    feat : add messa when error came from server
    - added message from server on form
    - added control to loading component

--- bookworm-app/bookworm/src/components/forms/LoginForm.js
+++ bookworm-app/bookworm/src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import PropTypes from 'prop-types';
 import InlineError from '../messages/InlineError'
@@ -28,7 +28,10 @@ class LoginForm extends React.Component {
         const errors = this.validate(this.state.data);
         this.setState({ errors });
         if (Object.keys(errors).length === 0) {
-            this.props.submit(this.state.data)
+            this.setState({ loading: true })
+            this.props
+                .submit(this.state.data)
+                .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
         }
     }
 
@@ -41,7 +44,11 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={this.state.loading}>
+                {this.state.errors.global && <Message negative>
+                    <Message.Header>Something went wrong</Message.Header>
+                    <p>{this.state.errors.global}</p>
+                </Message>}
                 <Form.Field error={!!this.state.errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
