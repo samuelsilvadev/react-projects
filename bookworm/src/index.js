120555c0bf93a1f27ac0f2d43b2137e343be6fce
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-19 09:42:17 -0300

    chore : install and setup redux
    - intalled redux, redux-react, redux-thunk
    - configured redux

--- bookworm/src/index.js
+++ bookworm/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
 import App from './App';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>,
     document.getElementById('root')
 );
