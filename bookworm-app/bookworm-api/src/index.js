8d303129148b058e22a3e3bc130c4d40457ff356
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-20 16:45:59 -0300

    chore : setup eslint
    - installed eslint and configured
    - configured .editorconfig

--- bookworm-app/bookworm-api/src/index.js
+++ bookworm-app/bookworm-api/src/index.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 
 app.post('/api/auth', (req, res) => {
-    res.status(400).json({
-        errors: {
-            global: 'Invalid credentials'
-        }
-    })
+	res.status(400).json({
+		errors: {
+			global: 'Invalid credentials',
+		},
+	});
 });
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(8080, () => console.log('Running on localhost:8080'));
