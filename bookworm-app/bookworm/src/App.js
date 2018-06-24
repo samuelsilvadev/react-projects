d40f299e972b6169700bfe80a3a75ea035091220
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-23 21:21:25 -0300

    feat : create dashboard page
    - created dashboard component
    - created confirmEmailComponent
    - updated routes

--- bookworm-app/bookworm/src/App.js
+++ bookworm-app/bookworm/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage'
 import LoginPage from './components/pages/LoginPage';
+import DashBoardPage from './components/pages/DashBoardPage';
 
 const App = () => (
   <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" component={LoginPage} />
+    <Route path="/dashboard" component={DashBoardPage} />
   </div>
 );
 
