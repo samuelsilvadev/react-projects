b08c430e2aed22a2b594ecfc7b0e2a42727c3638
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-29 08:04:20 -0300

    fix : user is not logged after signup if page is reloaded
    - added token on localstorage

--- bookworm-app/bookworm/src/actions/users.js
+++ bookworm-app/bookworm/src/actions/users.js
@@ -2,6 +2,9 @@ import api from '../api';
 import { userLoggedIn } from './auth';
 
 const signup = data => dispatch =>
-    api.user.signup(data).then(user => dispatch(userLoggedIn(user)));
+    api.user.signup(data).then(user => {
+        localStorage.bookwormJWT = user.token;
+        dispatch(userLoggedIn(user));
+    });
 
 export default signup;
