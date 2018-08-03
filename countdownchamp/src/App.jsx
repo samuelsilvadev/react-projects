ba2b4c22c58c999d3969ae5d07e6de93269579eb
Samuel silva <samuelsilvawb@gmail.com>
2018-08-03 15:21:07 -0300

    feat : change deadline in onclick
    - created function to update state
    - applied padding to app component

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'February 25, 2018',
+            newDeadline: '',
+        };
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App__header">
                     <h1>Countdown for</h1>
                     <div className="App__event">
-                        ...
+                        <p>{this.state.deadline}</p>
                     </div>
                 </header>
                 <section className="Clock">
@@ -17,14 +26,28 @@ class App extends React.Component {
                     <div className="Clock__minutes">50 minutes</div>
                     <div className="Clock__seconds">5 seconds</div>
                 </section>
-                <section className="Add-Event"> 
+                <section className="Add-Event">
                     <form className="Add-Event__form">
-                        <input type="text"/>
-                        <button>Send</button>
+                        <input type="text" onChange={this._handleOnChangeDeadline.bind(this)}/>
+                        <button onClick={this._setNewDeadLine.bind(this)}>Send</button>
                     </form>
                 </section>
             </div>
         );
+    }
+
+    _handleOnChangeDeadline(e) {
+        const valueTyped = e.target.value;
+        this.setState({
+            newDeadline: valueTyped,
+        })
+    }
+
+    _setNewDeadLine(e) {
+        e.preventDefault();
+        this.setState({
+            deadline: this.state.newDeadline
+        });
     }
 }
 
