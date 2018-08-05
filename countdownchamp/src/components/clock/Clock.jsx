d8ec5378de7395148aff01682b30aa6d1cabf032
Samuel silva <samuelsilvawb@gmail.com>
2018-08-05 06:20:10 -0300

    feat : show decrement time after pass a deadline
    - created logic to show time

--- countdownchamp/src/components/clock/Clock.jsx
+++ countdownchamp/src/components/clock/Clock.jsx
@@ -7,6 +7,7 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            deadline: '',
             days: 0,
             hours: 0,
             minutes: 0,
@@ -14,12 +15,18 @@ class Clock extends React.Component {
         };
     }
 
-    componentDidMount() { }
+    componentWillMount() {
+        this.setState(this._getTimeUntil(this.props.deadline));
+    }
+
+    componentDidMount() {
+        setInterval(() => this.setState(this._getTimeUntil(this.props.deadline)), 1000);
+    }
 
     render() {
         return (
             <section className="Clock">
-                <div className="Clock__days"> {this._formatTextToPlural('day', this.state.days)}</div>
+                <div className="Clock__days">{this._formatTextToPlural('day', this.state.days)}</div>
                 <div className="Clock__hours">{this._formatTextToPlural('hour', this.state.hours)}</div>
                 <div className="Clock__minutes">{this._formatTextToPlural('minute', this.state.minutes)}</div>
                 <div className="Clock__seconds">{this._formatTextToPlural('second', this.state.seconds)}</div>
@@ -27,7 +34,23 @@ class Clock extends React.Component {
         );
     }
 
-    componentWillUnmount() { }
+    _getTimeUntil(deadline) {
+
+        if (!deadline) { return; }
+
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        const seconds = Math.floor((time / 1000) % 60);
+        const minutes = Math.floor((time / 1000 / 60) % 60);
+        const hours = Math.floor(time / (1000 * 60 * 60) % 24);
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+        return {
+            seconds,
+            minutes,
+            hours,
+            days
+        }
+    }
 
     _formatTextToPlural(str, qtde) {
         return `${qtde} ${(qtde && qtde > 1) ? str + 's' : str}`;
