90b7f19456561dd91e9ee8b21a6b5a79d083da20
Samuel silva <samuelsilvawb@gmail.com>
2018-08-14 08:01:19 +0100

    chore : start project music-master-spotify
    - installed dependencies
    - started project
    - created main file and first component

---
+++ music-master-spotify/src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Music Master from App</div>
+        );
+    }
+}
+
+export default App;
