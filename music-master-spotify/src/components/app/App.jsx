435a2a1ed37fd5ac10b5cdbdfea346e33dbdb1c3
Samuel silva <samuelsilvawb@gmail.com>
2018-08-17 10:25:56 +0100

    chore : create state to keep what have been typed
    - initiated state
    - created functions to handle with user actions

--- music-master-spotify/src/components/app/App.jsx
+++ music-master-spotify/src/components/app/App.jsx
@@ -6,6 +6,31 @@ import Profile from '../profile/Profile';
 import Gallery from '../gallery/Gallery';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+        };
+        this._changeValueField = this._changeValueField.bind(this);
+        this._search = this._search.bind(this);
+        this._handleEnterKeyPessed = this._handleEnterKeyPessed.bind(this);
+    }
+
+    _changeValueField(e) {
+        this.setState({ query: e.target.value });
+    }
+
+    _search() {
+        console.log(this.state);
+    }
+
+    _handleEnterKeyPessed(e) {
+        if (e.key === 'Enter') {
+            this._search();
+        }
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,8 +40,11 @@ class App extends React.Component {
                         <FormControl
                             type="text"
                             placeholder="Search for an Artist"
+                            query={this.state.query}
+                            onChange={this._changeValueField}
+                            onKeyPress={this._handleEnterKeyPessed}
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={this._search}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
