de6ed5a986c1a2d59d93771938775327d3dfc49b
Samuel silva <samuelsilvawb@gmail.com>
2018-08-19 10:46:18 +0100

    chore : make simple request to endpoint of spotify's search
    - added new variable on .env file
    - created function to make request

--- music-master-spotify/src/components/app/App.jsx
+++ music-master-spotify/src/components/app/App.jsx
@@ -22,13 +22,32 @@ class App extends React.Component {
     }
 
     _search() {
-        console.log(this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        window.fetch(FETCH_URL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': process.env.REACT_APP_SPOTIFY_TOKEN,
+            }
+        })
+        .then(this._handleResponseSearch)
+        .then(console.log)
+        .catch(this._handleErrorResponseSearch);
     }
 
     _handleEnterKeyPessed(e) {
         if (e.key === 'Enter') {
             this._search();
         }
+    }
+
+    _handleResponseSearch(resp) {
+        return resp.json();
+    }
+
+    _handleErrorResponseSearch(error) {
+        console.error(error);
     }
 
     render() {
