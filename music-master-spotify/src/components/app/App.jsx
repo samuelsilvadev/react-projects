8bf6a3c693f4e7d61d9971ad5a74b8102ebf5f11
Samuel silva <samuel.silva@mindera.com>
2018-08-21 07:10:01 +0100

    chore : improve code and add aria attibutes
    - added aria attributes on App component
    - formatted css code

--- music-master-spotify/src/components/app/App.jsx
+++ music-master-spotify/src/components/app/App.jsx
@@ -6,84 +6,86 @@ import Profile from '../profile/Profile';
 import Gallery from '../gallery/Gallery';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: '',
+			artist: null
+		};
+		this._changeValueField = this._changeValueField.bind(this);
+		this._search = this._search.bind(this);
+		this._handleEnterKeyPressed = this._handleEnterKeyPressed.bind(this);
+		this._convertJsonToCorretObjectOnState = this._convertJsonToCorretObjectOnState.bind(this);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            query: '',
-            artist: null,
-        };
-        this._changeValueField = this._changeValueField.bind(this);
-        this._search = this._search.bind(this);
-        this._handleEnterKeyPessed = this._handleEnterKeyPessed.bind(this);
-        this._convertJsonToCorretObjectOnState = this._convertJsonToCorretObjectOnState.bind(this);
-    }
+	render() {
+		return (
+			<main role="main" className="App">
+				<h1 className="App__title">Music Master</h1>
+				<FormGroup role="search">
+					<InputGroup>
+						<FormControl
+							role="textbox"
+							type="text"
+							placeholder="Search for an Artist"
+							aria-label="Search for an Artist"
+							query={this.state.query}
+							onChange={this._changeValueField}
+							onKeyPress={this._handleEnterKeyPressed}
+							aria-required="true"
+							required
+						/>
+						<InputGroup.Addon role="button" onClick={this._search}>
+							<Glyphicon glyph="search" />
+						</InputGroup.Addon>
+					</InputGroup>
+				</FormGroup>
+				{this.state.artist && <Profile artist={this.state.artist} />}
+				<Gallery />
+			</main>
+		);
+	}
 
-    _changeValueField(e) {
-        this.setState({ query: e.target.value });
-    }
+	_changeValueField(e) {
+		this.setState({ query: e.target.value });
+	}
 
-    _search() {
-        if(this.state.query) {
-            const BASE_URL = 'https://api.spotify.com/v1/search?';
-            const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-            window.fetch(FETCH_URL, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': process.env.REACT_APP_SPOTIFY_TOKEN,
-                }
-            })
-            .then(this._handleResponseSearch)
-            .then(this._convertJsonToCorretObjectOnState)
-            .catch(this._handleErrorResponseSearch);
-        }
-    }
+	_search() {
+		if (this.state.query) {
+			const BASE_URL = 'https://api.spotify.com/v1/search?';
+			const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+			window
+				.fetch(FETCH_URL, {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: process.env.REACT_APP_SPOTIFY_TOKEN
+					}
+				})
+				.then((resp) => resp.json())
+				.then(this._convertJsonToCorretObjectOnState)
+				.catch(this._handleErrorResponseSearch);
+		}
+	}
 
-    _handleEnterKeyPessed(e) {
-        if (e.key === 'Enter') {
-            this._search();
-        }
-    }
+	_handleEnterKeyPressed(e) {
+		if (e.key === 'Enter') {
+			this._search();
+		}
+	}
 
-    _convertJsonToCorretObjectOnState(bigJSON) {
-        const [artist] = bigJSON.artists.items;
-        this.setState({
-            artist
-        });
-    }
+	_convertJsonToCorretObjectOnState(bigJSON) {
+		if (bigJSON.artist) {
+			const [ artist ] = bigJSON.artists.items;
+			this.setState({
+				artist
+			});
+		}
+	}
 
-    _handleResponseSearch(resp) {
-        return resp.json();
-    }
-
-    _handleErrorResponseSearch(error) {
-        console.error(error);
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h1 className="App__title">Music Master</h1>
-                <FormGroup>
-                    <InputGroup>
-                        <FormControl
-                            type="text"
-                            placeholder="Search for an Artist"
-                            query={this.state.query}
-                            onChange={this._changeValueField}
-                            onKeyPress={this._handleEnterKeyPessed}
-                        />
-                        <InputGroup.Addon onClick={this._search}>
-                            <Glyphicon glyph="search"></Glyphicon>
-                        </InputGroup.Addon>
-                    </InputGroup>
-                </FormGroup>
-                { this.state.artist && <Profile artist={this.state.artist} /> }
-                <Gallery />
-            </div>
-        );
-    }
+	_handleErrorResponseSearch(error) {
+		console.error(error);
+	}
 }
 
 export default App;
