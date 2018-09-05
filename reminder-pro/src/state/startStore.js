118840599d04e26e2405d3860ed80b6a6c6a0eb4
Samuel Silva <samuelsilvawb@gmail.com>
2018-09-05 22:39:19 +0100

    feat(reminder-pro): save state on localstorage
    - created helper function to save and update storage

--- reminder-pro/src/state/startStore.js
+++ reminder-pro/src/state/startStore.js
@@ -1,8 +1,17 @@
 import { createStore } from 'redux';
 
+import { loadState, saveState } from '../localStorage';
+
 import reminders from './reducers';
 
-export default createStore(
+const store = createStore(
     reminders,
+    loadState(),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
+
+export default store;
