03b4575782399ed980a18b773e04a7ad8ac463a1
Samuel Silva <samuelsilvawb@gmail.com>
2018-09-11 07:17:54 +0100

    chore(go-coach): configure routes
    - downgrade dependencie router
    - configured main routes

--- go-coach/src/index.js
+++ go-coach/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
 import SignIn from './components/sign-in';
@@ -8,6 +9,10 @@ import SignUp from './components/sign-up';
 const $rootSelector = document.querySelector('#root');
 
 ReactDOM.render(
-    <App />,
+    <Router path="/" history={ browserHistory }>
+        <Route path="/app" component={ App }/>
+        <Route path="/signin" component={ SignIn }/>
+        <Route path="/signup" component={ SignUp }/>
+    </Router>,
     $rootSelector
 );
