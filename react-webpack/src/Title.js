9b55c0ded5ff306ec47e4f1411887529115b3715
Samuel Silva <samuelsilvawb@gmail.com>
2018-09-28 21:44:10 +0100

    chore(react-webpack): create main files to up a project in React 15.4
    - Created webpack.config.
    - Created small server to up a project.
    - Added some initial files.
    - Added dependencies to use this version.

---
+++ react-webpack/src/Title.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Title = React.createClass({
+	render: function () {
+		return (
+			<h1>Hello World React 15.4</h1>
+		);
+	}
+});
+
+export default Title;
---
+++ react-webpack/src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Title from './Title';
+
+ReactDOM.render(
+    <Title />,
+    document.querySelector('[data-js="root"]')
+);
