a0df9c05b33efe87ec6cd3fcae3ffedfcb3910ee
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-03 08:33:21 +0100

    chore(prototype-shopping-cart): Add counter component
    - Created counter component.
    - Installed bootstrap.

--- prototype-shopping-cart/src/components/app/App.js
+++ prototype-shopping-cart/src/components/app/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Counter from './../counter/Counter';
+
+import './App.css';
 
 const App = () => (
-    <section>
-        <h1> Starting the project... </h1>
+    <section className="shoppingCart">
+        <h1 className="shoppingCart__title">Shopping Cart</h1>
+        <Counter className="shoppingCart__counter" value={2}/>
+        <Counter className="shoppingCart__counter" />
+        <Counter className="shoppingCart__counter" />
     </section>
 );
 
---
+++ prototype-shopping-cart/src/components/counter/Counter.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Counter.css';
+
+class Counter extends React.Component {
+    state = {
+        value: this.props.value,
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <span
+                    className={this._getBadgeClasses()}>
+                    {this._formatCount()}
+                </span>
+                <button
+                    className="btn btn-primary btn-sm"
+                    onClick={this._handleClickIncrementBtn}>
+                    Increment
+                </button>
+            </div>
+        );
+    }
+
+    _handleClickIncrementBtn = () => {
+        let { value } = this.state;
+        this.setState({
+            value: ++value,
+        });
+    }
+
+    _formatCount() {
+        const { value } = this.state;
+        return (value === 0 ? 'Zero' : value);
+    }
+
+    _getBadgeClasses() {
+        const { value } = this.state;
+        return `counter__quantity m-2 badge badge-${value === 0 ? 'warning' : 'success'}`;
+    }
+}
+
+Counter.propTypes = {
+    value: PropTypes.number,
+};
+
+Counter.defaultProps = {
+    value: 0,
+}
+
+export default Counter;
