f8d555e1fbb82a9d5fb83e2bb401287d3ecde397
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-07 17:41:32 +0100

    test(prototype-shopping-cart): Improve test and add test to all methods
    - Improved test coverage.
    - Added tests to all functions.
    - Updated snapshots.

--- prototype-shopping-cart/src/__tests__/Counter.test.js
+++ prototype-shopping-cart/src/__tests__/Counter.test.js
@@ -4,25 +4,42 @@ import { shallow } from 'enzyme';
 import Counter from './../components/counter/Counter';
 
 describe('<Counter />', () => {
-    it('should render correctly and match snapshot', () => {
-        const CounterComponent = shallow(<Counter id={1} value={0} />);
-        const instance = CounterComponent.instance();
-        
-        expect(instance.props.value).toBe(0);
-        expect(CounterComponent).toMatchSnapshot();
-    });
+	it('should render correctly and match snapshot', () => {
+		const counterComponent = shallow(<Counter id={1} value={0} />);
+		const instance = counterComponent.instance();
 
-    it('should update state after `_handleClickIncrementBtn` be called', () => {
-        const CounterComponent = shallow(<Counter id={1} value={1} />);
-        const instance = CounterComponent.instance();
-        
-        expect(instance.props.value).toBe(1);
-    });
+		expect(instance.props.id).toBe(1);
+		expect(instance.props.value).toBe(0);
+		expect(counterComponent).toMatchSnapshot();
+	});
 
-    it('should return correct value after `_formatCount` be called', () => {
-        const CounterComponent = shallow(<Counter id={1} />);
-        const instance = CounterComponent.instance();
-        
-        expect(instance._formatCount()).toBe('Zero');
-    });
+	it('should call `_handleClickIncrementBtn` when click on increment button', () => {
+		const counterComponent = shallow(<Counter id={1} value={1} />);
+		const instance = counterComponent.instance();
+		instance._handleClickIncrementBtn = jest.fn(instance._handleClickIncrementBtn);
+		instance.forceUpdate()
+		counterComponent.update()
+		counterComponent.find('[data-test="counter-btn-increment"]').simulate('click');
+
+		expect(instance._handleClickIncrementBtn).toHaveBeenCalled();
+	});
+
+	it('should call `_handleClickDeleteBtn` when click on delete button', () => {
+		const counterComponent = shallow(<Counter id={2} value={100} />);
+		const instance = counterComponent.instance();
+		instance._handleClickDeleteBtn = jest.fn(instance._handleClickDeleteBtn);
+		instance.forceUpdate()
+		counterComponent.update()
+		counterComponent.find('[data-test="counter-btn-delete"]').simulate('click');
+
+		expect(instance._handleClickDeleteBtn).toHaveBeenCalled();
+	});
+
+	it('should return correct value after `_formatCount` be called', () => {
+		const counterComponent = shallow(<Counter id={1} />);
+		const instance = counterComponent.instance();
+		const ZERO = 'Zero';
+
+		expect(instance._formatCount()).toBe(ZERO);
+	});
 });
--- prototype-shopping-cart/src/__tests__/CounterContainer.test.js
+++ prototype-shopping-cart/src/__tests__/CounterContainer.test.js
@@ -3,32 +3,83 @@ import { shallow } from 'enzyme';
 
 import CounterContainer from '../components/counter/CounterContainer';
 
-describe('<CounterContainer />', () => {
-    it('should render correctly without props', () => {
-        const CounterContainerComponent = shallow(<CounterContainer />);
+const counters = [
+	{
+		id: 1,
+		value: 2,
+	},
+	{
+		id: 2,
+		value: 0,
+	},
+	{
+		id: 3,
+	}
+];
 
-        expect(CounterContainerComponent).toMatchSnapshot();
+describe('<CounterContainer />', () => {
+	it('should render correctly without props', () => {
+		const counterContainerComponent = shallow(<CounterContainer />);
+
+		expect(counterContainerComponent).toMatchSnapshot();
 	});
-	
+
 	it('should render correctly with props', () => {
-		const counters = [
-			{
-				id: 1,
-				value: 2,
-			},
-			{
-				id: 2,
-				value: 0,
-			},
-			{
-				id: 3,
-			}
-		];
-		const CounterContainerComponent = shallow(<CounterContainer />);
-		CounterContainerComponent.setState({ counters });
-		const instance = CounterContainerComponent.instance();
+		const counterContainerComponent = shallow(<CounterContainer />);
+		const instance = counterContainerComponent.instance();
+
+		counterContainerComponent.setState({ counters });
 
 		expect(instance.state.counters.length).toBe(counters.length);
-        expect(CounterContainerComponent).toMatchSnapshot();
-    });
+		expect(counterContainerComponent).toMatchSnapshot();
+	});
+
+	it('should call `_handleClickResetBtn` when click on reset button', () => {
+		const counterContainerComponent = shallow(<CounterContainer />);
+		const instance = counterContainerComponent.instance();
+
+		instance._handleClickResetBtn = jest.fn(instance._handleClickResetBtn);
+		instance.forceUpdate();
+		counterContainerComponent.update();
+		counterContainerComponent.find('[data-test="counter-container-btn-reset"]').simulate('click');
+
+		expect(instance._handleClickResetBtn).toHaveBeenCalled();
+	});
+
+	it('should execute `_handleClickIncrementBtn` correctly and update state', () => {
+		const counterContainerComponent = shallow(<CounterContainer />);
+		const instance = counterContainerComponent.instance();
+		const ID_COUNTER = 3;
+		
+		counterContainerComponent.setState({ counters });
+		instance.setState = jest.fn();
+		
+		instance._handleClickIncrementBtn(ID_COUNTER);
+
+		expect(instance.setState).toHaveBeenCalled();
+		expect(instance.state.counters.find(c => c.id === ID_COUNTER).value).toBe(1);
+	});
+
+	it('should execute `_removeCounter` correctly and update state', () => {
+		const counterContainerComponent = shallow(<CounterContainer />);
+		const instance = counterContainerComponent.instance();
+		const ID_COUNTER = 3;
+		
+		counterContainerComponent.setState({ counters });
+		instance._removeCounter(ID_COUNTER);
+
+		expect(instance.state.counters.find(c => c.id === ID_COUNTER)).toBeUndefined();
+	});
+
+	it('should execute `_resetAllCounters` correctly and update state', () => {
+		const counterContainerComponent = shallow(<CounterContainer />);
+		const instance = counterContainerComponent.instance();
+		
+		counterContainerComponent.setState({ counters });
+		instance._resetAllCounters();
+
+		const isAllZeros = instance.state.counters.every(c => c.value === 0);
+
+		expect(isAllZeros).toBe(true);
+	});
 });
