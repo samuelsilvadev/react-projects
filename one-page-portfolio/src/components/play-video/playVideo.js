0d6a63ef6b6613276be7f307fefa46b7c0a24349
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-15 08:46:29 +0100

    feat(one-page-portfolio): create play video button component
    - Created component with styles.

---
+++ one-page-portfolio/src/components/play-video/playVideo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './playVideo.css';
+
+const PlayVideo = () => {
+	return (
+		<a href="#" className="playVideo">Start Video</a>
+	);
+}
+
+export default PlayVideo;
