39742e7e60465b419dc17a050ad4e3f4ce54cbf6
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-17 21:43:22 +0100

    chore(one-page-portfolio): integrate normalize and reset.css
    - Added css in app component.

--- one-page-portfolio/src/components/app/app.js
+++ one-page-portfolio/src/components/app/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Header from './../header';
+
+import './reset.css';
+import 'normalize.css';
 import './app.css';
 
 const App = () => {
 	return (
 		<React.Fragment>
+			<Header />
 		</React.Fragment>
 	);
 }
