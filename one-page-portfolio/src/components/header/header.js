fef42a0e716496b71f74ef7fb227688e1fc2758a
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-20 08:11:43 +0100

    chore(one-page-portfolio): structure components on header
    - Created sidebar.
    - Added profile image component.
    - Added image to profile.

--- one-page-portfolio/src/components/header/header.js
+++ one-page-portfolio/src/components/header/header.js
@@ -1,20 +1,34 @@
 import React from 'react';
 
 import Button from './../button'
+import ProfileImage from './../profile-image'
+
+import srcProfileImage from './../../assets/profileImage.jpg'
 
 import './header.css';
+
+const THEME_PROFILE_IMAGE = {
+	wrapper: 'main-header__image',
+	image: '',
+};
 
 const Header = () => {
 	return (
 		<header className="main-header">
-			<h1 className="main-header__title">Samuel Silva</h1>
+			<section className="profile">
+				<h1 className="main-header__title">Portfolio</h1>
+				<ProfileImage src={ srcProfileImage } alt="Profile Image" theme={ THEME_PROFILE_IMAGE } />
+				<p className="main-header__sub-title">Be creative</p>
+			</section>
+			
 			<nav className="main-nav">
-				<a className="main-nav__link" href="#">About</a>
-				<a className="main-nav__link" href="#">Portfolio</a>
+				<a className="main-nav__link" href="#about">About</a>
+				<a className="main-nav__link" href="#portfolio">Portfolio</a>
 				<Button>
 					Download CV
 				</Button>
 			</nav>
+			
 		</header>
 	);
 }
