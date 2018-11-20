7d87428493b062325eb7e3628e00ad487dd5f148
Samuel Silva <samuelsilvawb@gmail.com>
2018-11-20 22:32:01 +0100

    chore(react-webpack): configure production build
    - Added config to webpack generate production's files.
    - Removed extract plugin from dev to maintain hot reload.
    - Added script on package.json.
    - Disable rule `import/no-extraneous-dependencies` from eslint.

--- react-webpack/webpack.prod.config.js
+++ react-webpack/webpack.prod.config.js
@@ -7,24 +7,25 @@ const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	devtool: 'source-map',
 
-	entry: [
-		'react-hot-loader/patch',
-		'webpack-dev-server/client?http://localhost:3000',
-		'webpack/hot/only-dev-server',
-		path.join(__dirname, 'src', 'index'),
-	],
+	entry: path.join(__dirname, 'src', 'index'),
 
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: '[name]-[hash].js',
-		publicPath: '',
 	},
 
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
 		new ExtractTextPlugin('[name]-[hash].css'),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"',
+			},
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: { warnings: false },
+		}),
 		new HtmlPlugin({
 			template: path.join(__dirname, '', 'index.html'),
 		}),
@@ -46,7 +47,10 @@ module.exports = {
 			{
 				test: /\.css$/,
 				exclude: /node_modules/,
-				loader: ['style-loader', 'css-loader'],
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader',
+				}),
 			},
 		],
 	},
