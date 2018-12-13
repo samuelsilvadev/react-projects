ba8c262d8809c001848c278424118070147a21b5
Samuel SIlva <samuelsilvawb@gmail.com>
2018-12-13 07:45:01 +0100

    chore(github-app): add more stories on storybook
    - Added stories from `Actions, Loader, Repos, Search, UserInfo` components.

--- github-app/.storybook/stories/index.js
+++ github-app/.storybook/stories/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
-import Loader from './../../src/components/loader';
+import { withKnobs, boolean, object } from '@storybook/addon-knobs';
+import withPadding from './../decorators/withPadding';
 
-storiesOf('Loader', module)
-    .add('default', () => {
-        return <Loader />;
-    });
+import {
+	Actions,
+	Loader,
+	Repos,
+	Search,
+	UserInfo
+} from './../../src/components';
+
+storiesOf('Components', module)
+	.addDecorator(withPadding)
+	.addDecorator(withKnobs)
+	.add('Actions', () => {
+		return (
+			<Actions
+				handleClickSeeRepos={action('getting repositories...')}
+				handleClickSeeStarred={action(
+					'getting starred repositories...'
+				)}
+			/>
+		);
+	})
+	.add('Loader', () => {
+		return <Loader />;
+	})
+	.add('Repositories', () => {
+		const props = {
+			title: 'Repositories',
+			repos: [
+				{
+					name: 'trip-to-whatever',
+					html_url: '#'
+				}
+			]
+		};
+
+		return <Repos {...props} />;
+	})
+	.add('Search', () => {
+		return (
+			<Search
+				handleSearch={action('Searching for something...')}
+				isDisabled={boolean('isDisabled', false)}
+			/>
+		);
+	})
+	.add('UserInfo', () => {
+		return (
+			<UserInfo userInfo={object('userInfo', {
+				userName: 'Samuel SIlva',
+				repos: 64,
+				followers: 36,
+				following: 119,
+				photo: 'https://avatars3.githubusercontent.com/u/13966565?v=4',
+				login: 'samuelsilvadev',
+			})}/>
+		);
+	});
---
+++ github-app/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actions } from './actions';
+export { default as Loader } from './loader';
+export { default as Repos } from './repos';
+export { default as Search } from './search';
+export { default as UserInfo } from './user-info';
