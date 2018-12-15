ed40c907fd7cdedf0b833597f59ea288a8b07cb5
Samuel SIlva <samuelsilvawb@gmail.com>
2018-12-15 21:16:03 +0100

    test(github-app): cover pagination generator with tests
    - Added unit test to function

---
+++ github-app/src/components/pagination/__tests__/pagination.test.js
@@ -0,0 +1,70 @@
+import pagination from './../pagination';
+
+describe('Pagination utility', () => {
+	it('should be a function', () => {
+		expect(typeof pagination).toBe('function');
+	});
+
+	it('should return [1] when be passed pagination({ total: 1, active: 1 })', () => {
+		const params = { total: 1, active: 1 };
+		const result = [1];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2] when be passed pagination({ total: 2, active: 1 })', () => {
+		const params = { total: 2, active: 1 };
+		const result = [1, 2];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2, 3, 4, 5] when be passed pagination({ total: 5, active: 1 })', () => {
+		const params = { total: 5, active: 1 };
+		const result = [1, 2, 3, 4, 5];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2, 3, "...", 6] when be passed pagination({ total: 6, active: 1 })', () => {
+		const params = { total: 6, active: 1 };
+		const result = [1, 2, 3, '...', 6];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2, 3, "...", 6] when be passed pagination({ total: 6, active: 2 })', () => {
+		const params = { total: 6, active: 2 };
+		const result = [1, 2, 3, '...', 6];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2, 3, 4, 5, 6] when be passed pagination({ total: 6, active: 3 })', () => {
+		const params = { total: 6, active: 3 };
+		const result = [1, 2, 3, 4, 5, 6];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, 2, 3, 4, 5, 6] when be passed pagination({ total: 6, active: 4 })', () => {
+		const params = { total: 6, active: 4 };
+		const result = [1, 2, 3, 4, 5, 6];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, "...", 4, 5, 6] when be passed pagination({ total: 6, active: 5 })', () => {
+		const params = { total: 6, active: 5 };
+		const result = [1, '...', 4, 5, 6];
+
+		expect(pagination(params)).toEqual(result);
+	});
+
+	it('should return [1, "...", 7, 8, 9, "...", 15] when be passed pagination({ total: 15, active: 8 })', () => {
+		const params = { total: 15, active: 8 };
+		const result = [1, '...', 7, 8, 9, '...', 15];
+
+		expect(pagination(params)).toEqual(result);
+	});
+});
