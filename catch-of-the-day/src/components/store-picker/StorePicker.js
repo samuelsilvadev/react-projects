60d7ed3cf635756de2bd43e3f3336611f3bf1760
Samuel SIlva <samuelsilvawb@gmail.com>
2019-01-30 20:11:31 +0100

    chore(catch-of-the-day): add navigation from store picker to store page
    - Added imperative navigation.

--- catch-of-the-day/src/components/store-picker/StorePicker.js
+++ catch-of-the-day/src/components/store-picker/StorePicker.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 import './StorePicker.css';
 
-const StorePicker = () => {
-	return (
-		<form className="store-selector">
-			<h2>Please Enter a Store</h2>
-			<input className="store-name" type="text" name="store-name" required placeholder="Store Name" />
-			<button className="store-button" type="submit">Visit Store →</button>
-		</form>
-	);
+class StorePicker extends React.PureComponent {
+	constructor(props) {
+		super(props);
+
+		this.inputRef = React.createRef();
+
+		this._handleSubmit = this._handleSubmit.bind(this);
+	}
+
+	render() { 
+		return (
+			<form className="store-selector" onSubmit={ this._handleSubmit }>
+				<h2>Please Enter a Store</h2>
+				<input
+					ref={ this.inputRef }
+					className="store-name"
+					type="text"
+					name="store-name"
+					required
+					placeholder="Store Name"
+				/>
+				<button className="store-button" type="submit">Visit Store →</button>
+			</form>
+		);
+	}
+
+	_handleSubmit(event) {
+		event.preventDefault();
+		
+		if (this.inputRef && this.inputRef.current && this.inputRef.current.value) {
+			this.props.history.push(`/store/${this.inputRef.current.value}`)
+		}
+	}
 }
 
-export default StorePicker;
+StorePicker.propTypes = {
+	history: PropTypes.shape({
+		push: PropTypes.func.isRequired,
+	}),
+}
+
+export default withRouter(StorePicker);
