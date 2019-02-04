461c9cab191a06af0bbef42bd55e4f10ace2f8cc
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-04 07:58:06 +0100

    fix(catch-of-the-day): Change path images
    - Added new image path.

--- catch-of-the-day/src/data/fishes-mock.js
+++ catch-of-the-day/src/data/fishes-mock.js
@@ -1,7 +1,7 @@
 module.exports = {
 	fish1: {
 		name: 'Pacific Halibut',
-		image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
 		price: 1724,
 		status: 'available'
@@ -9,7 +9,7 @@ module.exports = {
 
 	fish2: {
 		name: 'Lobster',
-		image: 'http://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
 		price: 3200,
 		status: 'available'
@@ -17,7 +17,7 @@ module.exports = {
 
 	fish3: {
 		name: 'Sea Scallops',
-		image: 'http://i.istockimg.com/file_thumbview_approve/58624176/5/stock-photo-58624176-scallops-on-black-stone-plate.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
 		price: 1684,
 		status: 'unavailable'
@@ -25,7 +25,7 @@ module.exports = {
 
 	fish4: {
 		name: 'Mahi Mahi',
-		image: 'http://i.istockimg.com/file_thumbview_approve/12556651/5/stock-photo-12556651-mahimahi.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
 		price: 1129,
 		status: 'available'
@@ -33,7 +33,7 @@ module.exports = {
 
 	fish5: {
 		name: 'King Crab',
-		image: 'http://i.istockimg.com/file_thumbview_approve/18294110/5/stock-photo-18294110-king-crab-legs.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
 		price: 4234,
 		status: 'available'
@@ -41,7 +41,7 @@ module.exports = {
 
 	fish6: {
 		name: 'Atlantic Salmon',
-		image: 'http://i.istockimg.com/file_thumbview_approve/56241842/5/stock-photo-56241842-salmon-fish.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
 		price: 1453,
 		status: 'available'
@@ -49,7 +49,7 @@ module.exports = {
 
 	fish7: {
 		name: 'Oysters',
-		image: 'http://i.istockimg.com/file_thumbview_approve/58626682/5/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
 		price: 2543,
 		status: 'available'
@@ -57,7 +57,7 @@ module.exports = {
 
 	fish8: {
 		name: 'Mussels',
-		image: 'http://i.istockimg.com/file_thumbview_approve/40450406/5/stock-photo-40450406-steamed-mussels.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
 		price: 425,
 		status: 'available'
@@ -65,7 +65,7 @@ module.exports = {
 
 	fish9: {
 		name: 'Jumbo Prawns',
-		image: 'http://i.istockimg.com/file_thumbview_approve/67121439/5/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
+		image: 'https://placekitten.com/g/150/150',
 		desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
 		price: 2250,
 		status: 'available'
