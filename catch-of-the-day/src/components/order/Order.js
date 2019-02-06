791b2a57075776abddbc0e247de7e78206a3c7f0
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-06 06:29:37 +0100

    feat(catch-of-the-day): Add each order data in order component
    - Rendered order data to inform each order separately.

--- catch-of-the-day/src/components/order/Order.js
+++ catch-of-the-day/src/components/order/Order.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import './Order.css';
 
 class Order extends React.Component {
 	render() {
-		const { className } = this.props;
+		const { className, orders } = this.props;
+		
+		const ordersKeys = Object.keys(orders);
 
 		return (
 			<section className={ className }>
 				<h2>Orders</h2>
 				<ul className="orders-list">
+					{
+						ordersKeys.map(this._renderOrderItem, this)
+					}
 					<li className="orders-list__item orders-list__item--total">
 						<strong>Total:</strong>
 						{ this._getTotalPrice() }
 					</li>
 				</ul>
 			</section>
+		);
+	}
+
+	_renderOrderItem(key) {
+		const { orders, fishes } = this.props;
+
+		const fish = fishes[key];
+		const count = orders[key];
+		const isAvailable = fish && fish.status === 'available';
+
+		const content = !isAvailable ?
+			<p>Sorry, Fish is no longer available</p> :
+			<Fragment>
+				<p>{ count }lbs { fish.name }</p>
+				<p>{ count * fish.price }</p>
+			</Fragment>;
+
+		return (
+			<li className="orders-list__item" key={ key }>
+				{ content }
+			</li>
 		);
 	}
 
@@ -28,7 +54,7 @@ class Order extends React.Component {
 		const total = ordersKeys.reduce((prevValue, key) => {
 			const fish = fishes[key];
 			const count = orders[key];
-			const isAvailable = fish.status === 'available';
+			const isAvailable = fish && fish.status === 'available';
 
 			if (isAvailable) {
 				return prevValue + (count * fish.price || 0);
