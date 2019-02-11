3857d90cc6029af42b0bf902fa77b3fc0522d356
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-11 21:32:54 +0100

    feat(counter-with-redux): Add actions to increment and decrement
    - Added logic to make clicks works.

--- counter-with-redux/src/components/counter/Counter.js
+++ counter-with-redux/src/components/counter/Counter.js
@@ -3,15 +3,42 @@ import { connect } from 'react-redux';
 
 import './Counter.css';
 
-const Counter = () => {
-    return (
-        <div className="counter card">
-            <p className="counter__number">0</p>
-            <button className="counter__button btn waves-effect waves-light">-</button>
-            <button className="counter__button btn waves-effect waves-light">+</button>
-        </div>
-    );
-};
+const INCREMENT_TYPE = { type: 'INCREMENT' };
+const DECREMENT_TYPE = { type: 'DECREMENT' };
+
+class Counter extends React.PureComponent {
+    render() { 
+        const { counter } = this.props;
+
+        return (
+            <div className="counter card">
+                <p className="counter__number">{ counter }</p>
+                <button
+                    className="counter__button btn waves-effect waves-light"
+                    onClick={ this._handleDecrement }>
+                    -
+                </button>
+                <button
+                    className="counter__button btn waves-effect waves-light"
+                    onClick={ this._handleIncrement }>
+                    +
+                </button>
+            </div>
+        );
+    }
+
+    _handleIncrement = () => {
+        const { dispatch } = this.props; 
+
+        dispatch(INCREMENT_TYPE);
+    }
+
+    _handleDecrement = () => {
+        const { dispatch } = this.props; 
+
+        dispatch(DECREMENT_TYPE);
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
