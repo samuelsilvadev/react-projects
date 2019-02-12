825910af91a2edcdbdf20afa7c0cba8bf669f31f
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-12 08:10:34 +0100

    refactor(counter-with-redux): Create a folder to keep state
    - Created separated folder to organize state.

--- counter-with-redux/src/index.js
+++ counter-with-redux/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import 'materialize-css/dist/css/materialize.min.css';
 
+import { store } from './state';
+
 import './index.css';
 import App from './App';
-
-const reducer = (state = 0, action = {}) => {
-    switch (action.type) {
-        case 'INCREMENT': return state + 1;
-        case 'DECREMENT': return state - 1;
-        default: return state;
-    }
-}
-
-const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={ store }>
