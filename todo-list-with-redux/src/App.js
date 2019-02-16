6e4df28f7aedd23a9ecb9ea2a725f56c8cafa203
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-16 22:15:37 +0100

    chore(todo-list-with-redux): Create markup and workflow to add a todo
    - Added HTML.
    - Added render functions.
    - Added logic to add a todo on state as well.

--- todo-list-with-redux/src/App.js
+++ todo-list-with-redux/src/App.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addTodo } from './state/actions-creators';
 
 class App extends Component {
 	render() {
+		const { todos } = this.props;
+
 		return (
-			<div>
-				<h1>Starting...</h1>
-			</div>
+			<main>
+				<form onSubmit={ this._handleSubmit }>
+					<input
+						type="text"
+						name="todo"
+						autoComplete="off"
+						autoCapitalize="off" />
+					<button type="submit">Save</button>
+				</form>
+				<ul>
+					{ todos.map(this._renderTodoItem, this) }
+				</ul>
+			</main>
 		);
+	}
+
+	_renderTodoItem(todo) {
+		const { id, text } = todo;
+
+		return (
+			<li key={ id }>
+				{ text }
+			</li>
+		);
+	}
+
+	_handleSubmit = (event) => {
+		event.preventDefault();
+
+		const { addTodo } = this.props;
+		const text = event.target.todo.value;
+
+		addTodo(text);
+
+		event.target.reset();
 	}
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+	todos: state,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	addTodo: (text) =>  dispatch(addTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
