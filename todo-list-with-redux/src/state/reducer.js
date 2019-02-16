e3602b635cf8900fd3fa3fc6f58d2492e4af4605
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-16 20:10:44 +0100

    chore(todo-list-with-redux): Add todos reducer
    - Created todos reducer.
    - Created types to use on reducer function.

---
+++ todo-list-with-redux/src/state/reducer.js
@@ -0,0 +1,30 @@
+import { ADD_TODO, TOGGLE_TODO } from './types';
+
+const INITIAL_STATE = [];
+
+const todos = (state = INITIAL_STATE, action  = {}) => {
+	if (action.type === ADD_TODO) {
+		return state.concat({
+			id: action.payload.id,
+			text: action.payload.text,
+			completed: false,
+		});
+	}
+
+	if (action.type === TOGGLE_TODO) {
+		return state.map((todo) => {
+			if (todo.id === action.payload.id) {
+				return {
+					...todo,
+					completed: true,
+				}
+			}
+
+			return todo;
+		});
+	}
+
+	return state;
+};
+
+export default todos;
