9606c79edb3d9712926f655819c5281b44964ecd
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-16 20:49:01 +0100

    chore(todo-list-with-redux): Add redux library
    - Installed redux and react-redux.
    - Configured store initialization.

--- todo-list-with-redux/src/index.js
+++ todo-list-with-redux/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
+import { store } from './state/store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
