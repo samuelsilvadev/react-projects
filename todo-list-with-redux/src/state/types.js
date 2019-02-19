c6c7cbf5ac5501cfaa82346c550c2fc5529e424a
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-19 21:39:11 +0100

    refactor(todo-list-with-redux): Update action names
    - Added prefix on actions names to facilitate debug and maintain organized.

--- todo-list-with-redux/src/state/types.js
+++ todo-list-with-redux/src/state/types.js
@@ -1,6 +1,6 @@
-export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-export const SHOW_ALL = 'SHOW_ALL';
-export const SHOW_COMPLETED = 'SHOW_COMPLETED';
-export const SHOW_ACTIVE = 'SHOW_ACTIVE';
+export const ADD_TODO = 'todos:ADD_TODO';
+export const TOGGLE_TODO = 'todos:TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'visibilityFilter:SET_VISIBILITY_FILTER';
+export const SHOW_ALL = 'visibilityFilter:SHOW_ALL';
+export const SHOW_COMPLETED = 'visibilityFilter:SHOW_COMPLETED';
+export const SHOW_ACTIVE = 'visibilityFilter:SHOW_ACTIVE';
