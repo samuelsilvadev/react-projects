9865e5aaabb3fd16745d5bcc5bd077ddac33891f
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-20 07:10:23 +0100

    refactor(todo-list-with-redux): Connect filter with store
    - Connected filter on store.
    - Added array to render each filter.

--- todo-list-with-redux/src/components/filter/Filter.js
+++ todo-list-with-redux/src/components/filter/Filter.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { SHOW_ACTIVE, SHOW_ALL, SHOW_COMPLETED } from './../../state/types';
 
 import './Filter.css';
 
-export const Filter = () => {
+const FILTERS =  [
+	{ label: 'All', action: SHOW_ALL },
+	{ label: 'Completed', action: SHOW_COMPLETED },
+	{ label: 'Todo', action: SHOW_ACTIVE },
+];
+
+export const Filter = ({ activeFilter }) => {
+	const _renderFilterItem = (filter) => {
+		const isDisabled = activeFilter === filter.action;
+
+		return (
+			<button key={ filter.action } className='btn' disabled={ isDisabled }>{ filter.label }</button>
+		);
+	};
+
 	return (
 		<section>
 			<h2>Filter by:</h2>
-			<button className='btn' disabled>All</button>
-			<button className='btn'>Completed</button>
-			<button className='btn'>Todo</button>
+			{ FILTERS.map(_renderFilterItem) }
 		</section>
 	);
-}
+};
 
-export default Filter;
+const mapStateToProps = (state) => ({
+	activeFilter: state.visibilityFilters,
+});
+
+export default connect(mapStateToProps)(Filter);
