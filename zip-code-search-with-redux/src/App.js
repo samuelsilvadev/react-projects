9ca47c7dbe93046aefae94f1d32dbba8fe90ae0e
Samuel SIlva <samuelsilvawb@gmail.com>
2019-02-26 08:27:10 +0100

    chore(zip-code-search-with-redux): Create markup and initial style
    - Added markup.
    - Added initial style.

--- zip-code-search-with-redux/src/App.js
+++ zip-code-search-with-redux/src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 
 class App extends Component {
 	render() {
-		return <h1>Starting</h1>;
+		return (
+			<main className="wrapper">
+				<h1 className="wrapper__title">Search informations about your zip code	</h1>
+				<form className="search-form">
+					<input type="search" />
+					<button className="btn">Search</button>
+				</form>
+				<table className="search-results">
+					<thead>
+						<tr>
+							<td>ZIP</td>
+							<td>Adress</td>
+							<td>Neighbourhood</td>
+							<td>City</td>
+							<td>State</td>
+						</tr>
+					</thead>
+					<tbody>
+					</tbody>
+				</table>
+			</main>
+		);
 	}
 }
 
