98bfad2089c59d9f61b3181e6fd9f78db1394520
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-01 06:48:12 +0000

    feat(zip-code-search-with-redux): Make search by zip code works
    - Added submit handler when user search for some zip code.

--- zip-code-search-with-redux/src/App.js
+++ zip-code-search-with-redux/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.css';
@@ -11,38 +11,52 @@ const INITIAL_STATE = {
 	code: '',
 	district: '',
 	state: '',
-	status: 1
+	status: 0
 };
-
-async function fetchZipCode(callback) {
-	const apiWithZipCode = API.replace(ZIP_CODE_REGEX, '62870000');
-
-	try {
-		const response = await axios.get(apiWithZipCode);
-		
-		callback && callback(response.data);
-	} catch (error) {
-		console.error('Something wen wrong...', error);
-	}
-}
 
 const App = () => {
 	const [zipInformation, setZipInformation] = useState(INITIAL_STATE);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		fetchZipCode(setZipInformation);
-	}, []);
+		setIsLoading(false);
+	}, [zipInformation]);
 
-	const { address, city, code, district, state } = zipInformation;
+	const { address, city, code, district, state, status } = zipInformation;
+
+	function _handleSubmit(event) {
+		event.preventDefault();
+
+		const zipCode = event.target['zip-code'].value;
+
+		setIsLoading(true);
+		fetchZipCode(zipCode);	
+	};
+
+	async function fetchZipCode(zipCode) {
+		const apiWithZipCode = API.replace(ZIP_CODE_REGEX, zipCode);
+	
+		try {
+			const response = await axios.get(apiWithZipCode);
+			
+			setZipInformation(response.data);
+		} catch (error) {
+			console.error('Something wen wrong...', error);
+		}
+	}
 
 	return (
 		<main className='wrapper'>
 			<h1 className='wrapper__title'>
-				Search informations about your zip code{' '}
+				Search informations about your zip code
 			</h1>
-			<form className='search-form'>
-				<input type='search' />
-				<button className='btn'>Search</button>
+			<form onSubmit={ _handleSubmit } className='search-form'>
+				<input type='search' name="zip-code" />
+				<button
+					className='btn'
+					disabled={ isLoading }>
+					Search
+				</button>
 			</form>
 			<table className='search-results'>
 				<thead>
@@ -54,15 +68,17 @@ const App = () => {
 						<td>State</td>
 					</tr>
 				</thead>
-				<tbody>
-					<tr>
-						<td>{ code }</td>
-						<td>{ address }</td>
-						<td>{ district }</td>
-						<td>{ city }</td>
-						<td>{ state }</td>
-					</tr>
-				</tbody>
+				{ !!status &&
+					<tbody>
+						<tr>
+							<td>{ code }</td>
+							<td>{ address }</td>
+							<td>{ district }</td>
+							<td>{ city }</td>
+							<td>{ state }</td>
+						</tr>
+					</tbody>
+				}
 			</table>
 		</main>
 	);
