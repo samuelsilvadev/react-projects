76fa03fd36d261d17dd992e974872343f946fd82
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-02 21:10:34 +0100

    chore(zip-code-search-with-redux): Install redux-thunk
    - Installed redux-thunk librarie.
    - Configured thunk using `applyMiddleware`.

--- zip-code-search-with-redux/src/state/store.js
+++ zip-code-search-with-redux/src/state/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/root';
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
