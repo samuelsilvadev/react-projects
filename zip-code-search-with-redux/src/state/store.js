16725d2af3e60640809dc4a1ec2358f38243d9b9
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-03 19:14:05 +0100

    chore(zip-code-search-with-redux): Enable redux devtools
    - Added enhancer to start redux devtools.

--- zip-code-search-with-redux/src/state/store.js
+++ zip-code-search-with-redux/src/state/store.js
@@ -1,8 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
-import { logger } from './middlewares/logger';
 
 import rootReducer from './reducers/root';
 
-export const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const enableReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+	window.__REDUX_DEVTOOLS_EXTENSION__ :
+	(data) => data; 
+
+const enhancer = compose(
+	applyMiddleware(thunk),
+	enableReduxDevTools()
+);
+
+export const store = createStore(rootReducer, enhancer);
