5f8885d5dbde903c663a5d1810014cf8855af7d3
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-05 19:02:12 +0100

    refactor(reactflix): Format code with new styleguide
    - Formatted files.

--- reactflix/config/webpack/webpack.prod.js
+++ reactflix/config/webpack/webpack.prod.js
@@ -4,19 +4,19 @@ import paths from './paths';
 import rules from './rules';
 
 module.exports = {
-    mode: 'production',
-    output: {
-        filename: `${paths.jsFolder}/[name].[hash].js`,
-        path: paths.outputPath,
-        chunkFilename: '[name].[chunkhash].js'
-    },
-    module: {
-        rules
-    },
-    plugins: [
-        new CleanWebpackPlugin([paths.outputPath.split('/').pop()], {
-            root: paths.root
-        })
-    ],
-    devtool: 'source-map'
+	mode: 'production',
+	output: {
+		filename: `${paths.jsFolder}/[name].[hash].js`,
+		path: paths.outputPath,
+		chunkFilename: '[name].[chunkhash].js'
+	},
+	module: {
+		rules
+	},
+	plugins: [
+		new CleanWebpackPlugin([paths.outputPath.split('/').pop()], {
+			root: paths.root
+		})
+	],
+	devtool: 'source-map'
 };
--- reactflix/webpack.config.js
+++ reactflix/webpack.config.js
@@ -5,8 +5,8 @@ const webpackMerge = require('webpack-merge');
 const common = require('./config/webpack/webpack.common');
 
 const envs = {
-    development: 'dev',
-    production: 'prod'
+	development: 'dev',
+	production: 'prod'
 };
 
 const env = envs[process.env.NODE_ENV || 'development'];
