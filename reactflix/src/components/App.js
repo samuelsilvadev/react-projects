4a4b412db90acc20160b8c035a04674b0566211b
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-05 20:15:07 +0100

    chore(reactflix): Create first component Css-in-JS
    - Created title on App.style.js.
    - Imported on App.js

--- reactflix/src/components/App.js
+++ reactflix/src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Title from './App.style';
+
 const App = () => (
 	<main>
-		<h1>React Flix</h1>
+		<Title>React Flix</Title>
 	</main>
 );
 
---
+++ reactflix/src/components/App.style.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const Title = styled.h1`
+	text-align: center;
+`;
+
+export default Title;
