ccd5ee87fa0be05eed2b917d2bc9732872650c7a
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-07 21:10:10 +0100

    chore(reactflix): Create base of header, main and footer
    - Created base components using styled from '@emotion/styled'.

--- reactflix/src/components/App.js
+++ reactflix/src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import Title from './App.style';
+import { Title, Header, Main, Footer } from './App.style';
 
 const App = () => (
-	<main>
-		<Title data-enzyme-id="app-title">React Flix</Title>
-	</main>
+	<Fragment>
+		<Header>
+			<Title data-enzyme-id="app-title">React Flix</Title>
+		</Header>
+		<Main />
+		<Footer>
+			<p>
+				{ new Date().getFullYear() }
+			</p>
+		</Footer>
+	</Fragment>
 );
 
 export default App;
--- reactflix/src/components/App.style.js
+++ reactflix/src/components/App.style.js
@@ -1,7 +1,20 @@
 import styled from '@emotion/styled';
 
-const Title = styled.h1`
+const HEADER_HEIGHT = '60px';
+const FOOTER_HEIGHT = '30px';
+
+export const Title = styled.h1`
 	text-align: center;
 `;
 
-export default Title;
+export const Header = styled.header`
+	height: ${HEADER_HEIGHT};
+`;
+
+export const Main = styled.main`
+	min-height: calc(100% - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT});
+`;
+
+export const Footer = styled.footer`
+	height: ${FOOTER_HEIGHT};
+`;
