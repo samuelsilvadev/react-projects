d395ccafb0d8b4f9a1da1a8abe43a4f4ce07d389
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-09 22:10:08 +0100

    feat(reactflix): Add keyboard control to close modal
    - Added listener to be fired after modal mounts.

--- reactflix/src/components/modal/Modal.js
+++ reactflix/src/components/modal/Modal.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Container, CloseStyled } from './Modal.style';
 
 const Modal = ({ onClose }) => {
+	const _handleKeyPress = (event) => {
+		if (event.key === 'Escape') {
+			onClose();
+		}
+	};
+	
+	useEffect(() => {
+		document.addEventListener('keydown', _handleKeyPress);
+
+		return () => {
+			document.removeEventListener('keydown', _handleKeyPress);
+		} ;
+	}, []);
+
 	return (
 		<Container>
 			<CloseStyled onClick={ onClose } />
