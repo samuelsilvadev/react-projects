542735f3c870415b51f34a6138a7e4f6b695f1f0
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-09 21:23:33 +0100

    chore(reactflix): Create variables to control z-index
    - Created variables.

--- reactflix/src/components/style.js
+++ reactflix/src/components/style.js
@@ -2,3 +2,10 @@ export const HEADER_HEIGHT = '60px';
 export const FOOTER_HEIGHT = '30px';
 export const BACKGROUND_COLOR = '#761D49';
 export const WHITE = '#fff';
+
+/**
+ * Layers
+*/
+
+export const MODAL = 2;
+export const GENERAL = 1;
