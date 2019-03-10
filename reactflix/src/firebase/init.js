e1352e842c93b751509c3b54c5993626706c6821
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-10 09:43:01 +0100

    chore(reactflix): Configure firebase
    - Installed dependencie.
    - Added init config file.

---
+++ reactflix/src/firebase/init.js
@@ -0,0 +1,16 @@
+import app from 'firebase/app';
+
+import 'firebase/database';
+
+const config = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+const firebaseApp = app.initializeApp(config);
+
+export default firebaseApp;
