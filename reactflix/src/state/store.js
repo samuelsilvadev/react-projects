d8dc7edf59de18aa06a81d0cd058005ec34194ff
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-26 07:13:34 +0000

    chore(reactflix): Enable redux dev tools
    - Added enableReduxDevTools function as middleware.

--- reactflix/src/state/store.js
+++ reactflix/src/state/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/root';
 
-export const store = createStore(rootReducer);
+const enableReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+	window.__REDUX_DEVTOOLS_EXTENSION__ :
+	(data) => data; 
+
+export const store = createStore(rootReducer, enableReduxDevTools());
 
 export default store;
