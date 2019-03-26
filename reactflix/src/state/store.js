4d4f229f26caa924c59301f1cae7cab87270a23b
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-26 07:31:16 +0000

    chore(reactflix): Enable use of more than one middleware
    - Created enhancer.
    - Enabled redux-thunk.

--- reactflix/src/state/store.js
+++ reactflix/src/state/store.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import rootReducer from './reducers/root';
 
 const enableReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ ?
 	window.__REDUX_DEVTOOLS_EXTENSION__ :
 	(data) => data; 
 
-export const store = createStore(rootReducer, enableReduxDevTools());
+const enhancer = compose(
+	applyMiddleware(thunk),
+	enableReduxDevTools()
+);
+
+export const store = createStore(rootReducer, enhancer);
 
 export default store;
