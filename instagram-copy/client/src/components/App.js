17d232ac66dae77b6d13b5a40855c1cf35ffdfd0
Samuel SIlva <samuelsilvawb@gmail.com>
2019-04-02 07:37:01 +0100

    chore(instagram-copy-client): Integrate Header and Post Markup
    - Added Header and Post markup on home page.

--- instagram-copy/client/src/components/App.js
+++ instagram-copy/client/src/components/App.js
@@ -1,7 +1,18 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
+
+import { Main } from './App.style';
+
+import Header from './header';
+import Post from './post';
 
 const App = () => (
-	<h1>Instagram copy</h1>
+	<Fragment>
+		<Header />
+		<Main>
+			<Post />
+			<Post />
+		</Main>
+	</Fragment>
 );
 
 export default App;
---
+++ instagram-copy/client/src/components/App.style.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+import { NAVIGATION_HEIGHT } from './style';
+
+export const Main = styled.main`
+	margin-top: ${NAVIGATION_HEIGHT};
+`;
+
+export default Main;
