920e8fae8fb6f5ce36b0fb69909f4b0d07d0ba43
Samuel SIlva <samuelsilvawb@gmail.com>
2019-04-22 07:40:02 +0100

    feat(financial-control-server): Create route that aggregate debs and credits
    - Added `summary` route.

--- financial-control/server/src/controllers/billingCycleController.js
+++ financial-control/server/src/controllers/billingCycleController.js
@@ -14,4 +14,36 @@ BillingCycle.route('count', (req, res, _next) => {
 	});
 });
 
+// eslint-disable-next-line no-unused-vars
+BillingCycle.route('summary', (req, res, next) => {
+	BillingCycle.aggregate([{
+		$project: {
+			credit: {
+				$sum: '$credits.value',
+			},
+			debt: {
+				$sum: '$debts.value',
+			},
+		},
+	}, {
+		$group: {
+			_id: null,
+			credit: { $sum: '$credit' },
+			debt: { $sum: '$debt' },
+		},
+	}, {
+		$project: {
+			_id: 0,
+			credit: 1,
+			debt: 1,
+		},
+	}], (error, result) => {
+		if (error) {
+			res.status(500).json({ errors: [error] });
+		} else {
+			res.json(result[0] || { credit: 0, debt: 0 });
+		}
+	});
+});
+
 module.exports = BillingCycle;
