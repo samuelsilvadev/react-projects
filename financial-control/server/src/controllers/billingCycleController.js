43a47b082c39dc7319b564b0fd672ad97bdc4e7a
Samuel SIlva <samuelsilvawb@gmail.com>
2019-04-22 06:58:02 +0100

    feat(financial-control-server): Create route to count registers
    - Added `count` route.

--- financial-control/server/src/controllers/billingCycleController.js
+++ financial-control/server/src/controllers/billingCycleController.js
@@ -3,4 +3,15 @@ const BillingCycle = require('./../models/billingCycle');
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+// eslint-disable-next-line no-unused-vars
+BillingCycle.route('count', (req, res, _next) => {
+	BillingCycle.count((error, value) => {
+		if (error) {
+			res.status(500).json({ errors: [error] });
+		} else {
+			res.json({ value });
+		}
+	});
+});
+
 module.exports = BillingCycle;
