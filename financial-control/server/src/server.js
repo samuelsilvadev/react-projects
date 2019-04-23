093331669be72a2f374ea7885caebcebeccc3ee7
Samuel SIlva <samuelsilvawb@gmail.com>
2019-04-23 06:12:10 +0100

    chore(financial-control-server): Enable `express-query-int`
    - Enabled middleware `queryParser`.

--- financial-control/server/src/server.js
+++ financial-control/server/src/server.js
@@ -5,12 +5,14 @@ const PORT = 3003;
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const queryParser = require('express-query-int');
 const server = express();
 
 const cors = require('./middlewares/cors');
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(queryParser());
 server.use(cors);
 
 require('./routes')(server);
