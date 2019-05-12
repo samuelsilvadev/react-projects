1fd6fc0d7d7e457847db48b3a37b6df95781067d
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-12 11:53:10 +0100

    chore(financial-control-client): Configure redux-thunk
    - Applied redux-thunk middleware.

--- financial-control/client/src/shared/state/store.js
+++ financial-control/client/src/shared/state/store.js
@@ -1,12 +1,20 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './rootReducer';
+
+const middlewares = [
+	thunk
+];
 
 const enableReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ ?
 	window.__REDUX_DEVTOOLS_EXTENSION__ :
 	(data) => data;
 
-const enhancer = compose(enableReduxDevTools());
+const enhancer = compose(
+	applyMiddleware(...middlewares),
+	enableReduxDevTools()
+);
 
 export const store = createStore(rootReducer, enhancer);
 
