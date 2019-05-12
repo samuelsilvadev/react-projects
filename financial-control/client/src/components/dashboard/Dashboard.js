31bd28cf39b39d57d2ce02ae83113059080d82b7
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-12 13:23:36 +0100

    feat(financial-control-client): Integrate redux into dashboard
    - Integrate state with Dashboard component.

--- financial-control/client/src/components/dashboard/Dashboard.js
+++ financial-control/client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { H1, GridDiv, StyledValueBox } from './Dashboard.style';
 
-export function Dashboard() {
+import { loadSummary } from '@shared/state/dashboard/actions';
+
+export function Dashboard(props) {
+	useEffect(() => {
+		const { loadSummary } = props;
+
+		loadSummary();
+	}, []);
+
+	const { summary: { data: { credit = 0, debt = 0 } = {} } } = props;
+
 	return (
 		<Fragment>
 			<H1>Dashboard</H1>
 			<GridDiv>
-				<StyledValueBox type="credit" title="$ 10.000" text="Credits Total"/>
-				<StyledValueBox type="debit" title="$ 10" text="Debts Total"/>
-				<StyledValueBox type="consolidated" title="$ 0" text="Consolidated Value"/>
+				<StyledValueBox type="credit" title={ `$ ${credit}` } text="Credits Total"/>
+				<StyledValueBox type="debit" title={ `$ ${debt}` } text="Debts Total"/>
+				<StyledValueBox type="consolidated" title={ `$ ${credit - debt}` } text="Consolidated Value"/>
 			</GridDiv>
 		</Fragment>
 	)
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+	summary: PropTypes.shape({
+		data: PropTypes.shape({
+			credit: PropTypes.number,
+			debt: PropTypes.number,
+		}),
+		isLoading: PropTypes.boolean,
+		error: PropTypes.any,
+	}),
+	loadSummary: PropTypes.func,
+};
+
+function mapStateToProps(state) {
+	return {
+		summary: state.dashboard.summary,
+	}
+}
+
+const mapDispatchToProps = {
+	loadSummary,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
