e14bd9b495aabcdd675cb24103a985fa7f990ffb
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-12 13:22:09 +0100

    feat(financial-control-client): Create redux logic to get summary
    - Created action, reducer, and actionTypes.

---
+++ financial-control/client/src/shared/state/dashboard/actions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { BILLING_SUMMARY_LOADING, BILLING_SUMMARY_SUCCESS, BILLING_SUMMARY_ERROR } from './actionsTypes';
+
+export function loadSummary() {
+	return async function(dispatch) {
+		const END_POINT = `${process.env.SERVER_BASE_URL}/billing-cycle/summary`;
+		
+		dispatch({ type: BILLING_SUMMARY_LOADING })
+	
+		try {
+			const { data } = await axios.get(END_POINT);
+	
+			dispatch({ type: BILLING_SUMMARY_SUCCESS, payload: { data } });
+		} catch (error) {
+			dispatch({
+				type: BILLING_SUMMARY_ERROR,
+				payload: {
+					error: { 
+						error,
+						message: error.message
+					}
+				}
+			});
+		}
+	}
+}
--- financial-control/client/src/shared/state/dashboard/reducer.js
+++ financial-control/client/src/shared/state/dashboard/reducer.js
@@ -1,5 +1,42 @@
-function reducer(state = {}, action) {
-	return state;
+import { BILLING_SUMMARY_SUCCESS, BILLING_SUMMARY_LOADING, BILLING_SUMMARY_ERROR } from './actionsTypes';
+
+const INITIAL_STATE = {
+	summary: {
+		isLoading: false,
+		error: undefined,
+		data: {}
+	}
+};
+
+function reducer(state = INITIAL_STATE, action = {}) {
+	switch (action.type) {
+		case BILLING_SUMMARY_LOADING:
+			return {
+				summary: {
+					isLoading: true,
+					error: undefined,
+				},
+			}
+		case BILLING_SUMMARY_SUCCESS:
+			return {
+				summary: {
+					data: {
+						...action.payload.data,
+					},
+					isLoading: false,
+					error: undefined,
+				},
+			}
+		case BILLING_SUMMARY_ERROR:
+			return {
+				summary: {
+					isLoading: false,
+					error: action.payload.error,
+				},
+			}
+		default:
+			return state;
+	}
 }
 
 export default reducer;
