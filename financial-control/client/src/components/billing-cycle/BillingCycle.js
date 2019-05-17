4427582066bace392cd339365a3f27c7b5f7b321
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-17 06:38:41 +0100

    refactorfinancial-control-client): Add tabs in `BillingCycle` component
    - Added Tabs inside component.
    - Added BillingCycleList inside list tab too.

--- financial-control/client/src/components/billing-cycle/BillingCycle.js
+++ financial-control/client/src/components/billing-cycle/BillingCycle.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import { TabsProvider } from '@shared/context';
+import { Tabs, TabHeader, TabTitle, TabBody, TabContent } from '@components/tabs';
+
+import List from './BillingCycleList';
+
 export function BillingCycle() {
 	return (
-		<h1>BillingCycle</h1>
+		<TabsProvider>
+			<Tabs>
+				<TabHeader>
+					<TabTitle label="Register" id="register" />
+					<TabTitle label="List" id="list" />
+				</TabHeader>
+				<TabBody>
+					<TabContent id="register">
+						Register Content
+					</TabContent>
+					<TabContent id="list">
+						<List />
+					</TabContent>
+				</TabBody>
+			</Tabs>
+		</TabsProvider>
 	)
 }
 
