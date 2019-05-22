e6a1cb2295b2d75480c8af690033604c8be90710
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-22 06:10:11 +0100

    feat(financial-control-client): Connect reducer from redux-form
    - Added reducer into combineReducers.

--- financial-control/client/src/shared/state/rootReducer.js
+++ financial-control/client/src/shared/state/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import { dashboardReducer } from './dashboard';
 import { billingCycleReducer } from './billing-cycle';
@@ -6,6 +7,7 @@ import { billingCycleReducer } from './billing-cycle';
 const rootReducer = combineReducers({
 	dashboard: dashboardReducer,
 	billingCycle: billingCycleReducer,
+	form: formReducer,
 });
 
 export default rootReducer;
