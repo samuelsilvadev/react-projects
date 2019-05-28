da5f1e8cb2cec7e62b3a63dc125a5a2d21c862d5
Samuel SIlva <samuelsilvawb@gmail.com>
2019-05-28 06:34:55 +0100

    feat(financial-control-client): Prepare state to handle with edit actions
    - Created new actions types.
    - Added reducer generator to reuse same reducer to save and edit.

--- financial-control/client/src/shared/state/billing-cycle/reducer.js
+++ financial-control/client/src/shared/state/billing-cycle/reducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
 import {
 	BILLING_CYCLE_LOADING,
-	BILLING_CYCLE_SAVE_LOADING,
 	BILLING_CYCLE_SUCCESS,
 	BILLING_CYCLE_ERROR,
+	BILLING_CYCLE_SAVE_LOADING,
 	BILLING_CYCLE_SAVE_SUCCESS,
 	BILLING_CYCLE_SAVE_ERROR,
+	BILLING_CYCLE_EDIT_LOADING,
+	BILLING_CYCLE_EDIT_SUCCESS,
+	BILLING_CYCLE_EDIT_ERROR,
 } from './actionsTypes';
 
 const LIST_INITIAL_STATE = {
@@ -17,27 +20,39 @@ const REGISTER_INITIAL_STATE = {
 	isLoading: false,
 };
 
-export function registerReducer(state = REGISTER_INITIAL_STATE, action = {}) {
-	switch (action.type) {
-		case BILLING_CYCLE_SAVE_LOADING:
-			return {
-				...state,
-				isLoading: true,
-			}
-		case BILLING_CYCLE_SAVE_SUCCESS:
-			return {
-				...state,
-				isLoading: false,
-			}
-		case BILLING_CYCLE_SAVE_ERROR:
-			return {
-				...state,
-				isLoading: false,
-				error: action.payload.error,
-			}
-
-		default:
-			return state;
+/**
+ * Creates a reducer function.
+ * 
+ * @param {Object} actions Set of actions types.
+ * @param {String} actions.loading LOADING action type.
+ * @param {String} actions.success SUCCESS action type.
+ * @param {String} actions.error ERROR action type.
+ * 
+ * @returns {Function} reducer.
+ */
+function generateReducer({ loading, success, error }) {
+	return function reducer(state = REGISTER_INITIAL_STATE, action = {}) {
+		switch (action.type) {
+			case loading:
+				return {
+					...state,
+					isLoading: true,
+				}
+			case success:
+				return {
+					...state,
+					isLoading: false,
+				}
+			case error:
+				return {
+					...state,
+					isLoading: false,
+					error: action.payload.error,
+				}
+	
+			default:
+				return state;
+		}
 	}
 }
 
@@ -68,5 +83,14 @@ export function listReducer(state = LIST_INITIAL_STATE, action = {}) {
 
 export default combineReducers({
 	list: listReducer,
-	register: registerReducer
+	register: generateReducer({
+		loading: BILLING_CYCLE_SAVE_LOADING,
+		success: BILLING_CYCLE_SAVE_SUCCESS,
+		error: BILLING_CYCLE_SAVE_ERROR
+	}),
+	update: generateReducer({
+		loading: BILLING_CYCLE_EDIT_LOADING,
+		success: BILLING_CYCLE_EDIT_SUCCESS,
+		error: BILLING_CYCLE_EDIT_ERROR
+	}),
 });
