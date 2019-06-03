4b788873c262efe38c3b2b20ddc4d30ea0f4ea5d
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-03 06:48:20 +0100

    refactor(ssr): Change modules from CommonJs to ESModules
    - Updated main module to use ESModules.

--- ssr/server/src/index.js
+++ ssr/server/src/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
-const app = express();
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 
-const Home = require('./client/components/Home').default;
+import Home from './client/components/Home';
+
+const app = express();
 
 app.get('/', (req, res) => {
     const content = renderToString(<Home />);
