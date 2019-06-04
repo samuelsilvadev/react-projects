555346098daeab4793ac232ad0294cf3c188d3a6
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-04 07:10:41 +0100

    refactor(ssr): Create common webpack config
    - Created common config and merged with others configs files.

--- ssr/server/webpack.client.js
+++ ssr/server/webpack.client.js
@@ -1,32 +1,14 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const common = require('./webpack.common');
+
+const config = {
     entry: './src/client/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        'react',
-                        'stage-0',
-                        [
-                            'env',
-                            {
-                                targets: {
-                                    browsers: ['last 2 versions']
-                                }
-                            }
-                        ]
-                    ]
-                },
-            }
-        ],
-    },
 };
+
+module.exports = merge(common, config)
--- ssr/server/webpack.server.js
+++ ssr/server/webpack.server.js
@@ -1,33 +1,15 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const common = require('./webpack.common');
+
+const config = {
     target: 'node',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        'react',
-                        'stage-0',
-                        [
-                            'env',
-                            {
-                                targets: {
-                                    browsers: ['last 2 versions']
-                                }
-                            }
-                        ]
-                    ]
-                },
-            }
-        ],
-    },
 };
+
+module.exports = merge(common, config);
