7fd7119be68038d4cb9f91913df14e986b686c51
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-06 19:53:51 +0100

    refactor(ssr): Add rootReducer in store creation
    - Imported and added in store rootReducer.

--- ssr/server/src/client/index.js
+++ ssr/server/src/client/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
+
+import rootReducer from './state/reducers/rootReducer';
+
 import Routes from './Routes';
 
 const INITIAL_STATE = {};
 
-const store = createStore(() => {}, INITIAL_STATE, applyMiddleware(thunk));
+const store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
     <Provider store={ store }>
--- ssr/server/src/helpers/createStoreFactory.js
+++ ssr/server/src/helpers/createStoreFactory.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+import rootReducer from './../client/state/reducers/rootReducer';
+
 const INITIAL_STATE = {};
 
 export default function createStoreFactory() {
-    const store = createStore(() => {}, INITIAL_STATE, applyMiddleware(thunk));
+    const store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(thunk));
 
     return store;
 };
