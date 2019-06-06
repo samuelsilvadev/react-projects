1c69ebbaa7843c44675d1708505bf43282e25584
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-06 19:18:56 +0100

    feat(ssr): Add redux in client
    - Created initial structure to use redux on client.

--- ssr/server/src/client/index.js
+++ ssr/server/src/client/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
+const INITIAL_STATE = {};
+
+const store = createStore(() => {}, INITIAL_STATE, applyMiddleware(thunk));
+
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>,
+    <Provider store={ store }>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
