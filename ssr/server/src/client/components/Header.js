1d091864ddbc32b5afc7e07ca14c784d3ecdca45
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-12 06:29:38 +0100

    feat(ssr): Add buttons to sign-in and sign-out
    - Added buttons to sign-in and sign-out.
    - Added Admins link.

--- ssr/server/src/client/components/Header.js
+++ ssr/server/src/client/components/Header.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+function AuthButton({ isLogged }) {
+    return (
+        isLogged ?
+            <a href="/api/logout">Logout</a> :
+            <a href="/api/auth/google">Login</a>
+    );
+}
+
 export function Header(props) {
     const { isLogged } = props;
 
@@ -9,8 +17,11 @@ export function Header(props) {
         <header>
             <Link to="/">Home</Link>
             <Link to="/users">Users</Link>
-            { !isLogged && <Link to="/users">Login</Link> }
-            { isLogged && <Link to="/users">Logout</Link> }
+            {
+                isLogged &&
+                    <Link to="/admins">Admin</Link>
+            }
+            <AuthButton isLogged={ isLogged } />
         </header>
     )
 }
