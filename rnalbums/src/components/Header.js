4ba862b95f38c5fdd652eb025265f28de1057d13
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-18 09:13:49 +0100

    feat(rn-albums): Add Header component
    - Removed unnecessary code instructions.
    - Added Header component.

---
+++ rnalbums/src/components/Header.js
@@ -0,0 +1,44 @@
+/**
+ * @format
+ * @flow
+ */
+
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type Props = {
+    title: string,
+};
+
+function Header(props: Props) {
+    const { title } = props;
+
+    return (
+        <View style={ styles.view }>
+            <Text style={ styles.text }>{ title }</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    view: {
+        alignItems: 'center',
+        backgroundColor: '#f8f8f8',
+        elevation: 2,
+        height: 70,
+        justifyContent: 'center',
+        paddingTop: 25,
+        position: 'relative',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+    },
+    text: {
+        fontSize: 20,
+    }    
+});
+
+export default Header;
