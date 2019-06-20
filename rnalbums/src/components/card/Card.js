5a77104f0476ac1ec4f28874f57cece134dd3b5f
Samuel SIlva <samuelsilvawb@gmail.com>
2019-06-20 09:52:50 +0100

    feat(rn-albums): Create Card and CardSection components
    - Added Card and CardSection components.

---
+++ rnalbums/src/components/card/Card.js
@@ -0,0 +1,40 @@
+/**
+ * @format
+ * @flow
+ */
+
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {
+    children: React.Element<any>,
+}
+
+function Card(props: Props) {
+    const { children } = props;
+
+    return (
+        <View style={ styles.container }>
+            { children }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+    },
+});
+
+export default Card;
---
+++ rnalbums/src/components/card/CardSection.js
@@ -0,0 +1,35 @@
+/**
+ * @format
+ * @flow
+ */
+
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {
+    children: React.Element<any>,
+}
+
+function CardSection(props: Props) {
+    const { children } = props;
+
+    return (
+        <View style={ styles.container }>
+            { children }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        borderColor: '#ddd',
+        borderBottomWidth: 1,
+        padding: 5,
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        position: 'relative',
+    },
+});
+
+export default CardSection;
