3b0c77df8e90b4e047a0e0e825bbb717ca068989
Samuel Silva <samuelsilvawb@gmail.com>
2019-06-26 19:20:16 +0100

    chore(rnbasicauth): Configure firebase
    - Installed dependency.
    - Added config variables.

--- rnbasicauth/src/App.js
+++ rnbasicauth/src/App.js
@@ -14,6 +14,20 @@ import { Header } from './components/common';
 type Props = {};
 
 class App extends React.Component<Props> {
+  constructor() {
+    super();
+
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDVNeETygFhLf5djG41XbcGzaal_d-RzKo',
+      authDomain: 'react-native-auth-a8fcd.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-a8fcd.firebaseio.com',
+      projectId: 'react-native-auth-a8fcd',
+      storageBucket: '',
+      messagingSenderId: '154305943011',
+      appId: '1:154305943011:web:113acfbbf3b5731',
+    });
+  }
+
   render() {
     return (
       <View>
