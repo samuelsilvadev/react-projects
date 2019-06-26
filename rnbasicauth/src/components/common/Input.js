efce5c3bc77202d2b356263d91f957158dc4a5ad
Samuel Silva <samuelsilvawb@gmail.com>
2019-06-26 20:00:20 +0100

    feat(rnbasicauth): Create a custom Input
    - Created Input text component.

---
+++ rnbasicauth/src/components/common/Input.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
+
+type Props = {
+    label: string,
+    placeholder: string,
+    value: any,
+    onChangeText: Function,
+};
+
+export function Input(props: Props) {
+    const { label, placeholder, value, onChangeText, ...restProps } = props;
+
+    return (
+        <View style={ styles.container }>
+            <Text
+                style={ styles.label }>
+                { label }
+            </Text>
+            <TextInput
+                autoCorrect={ false }
+                style={ styles.textInput }
+                value={ value }
+                onChangeText={ onChangeText }
+                placeholder={ placeholder }
+                { ...restProps } />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    label: {
+        paddingLeft: 10,
+        fontSize: 18,
+        flex: 1,
+    },
+    textInput: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2,
+    },
+})
