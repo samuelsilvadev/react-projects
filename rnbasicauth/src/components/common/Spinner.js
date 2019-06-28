694322ffeef4def5e73517bec290cd395710893d
Samuel Silva <samuelsilvawb@gmail.com>
2019-06-28 07:31:13 +0100

    feat(rnbasicauth): Add Spinner component
    - Added generic spinner component.

---
+++ rnbasicauth/src/components/common/Spinner.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+
+type Props = {
+    size: String,
+};
+
+export function Spinner(props: Props) {
+    return (
+        <View style={ styles.spinner }>
+            <ActivityIndicator size={ props.size || 'large' } />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    spinner: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+    }
+});
+
