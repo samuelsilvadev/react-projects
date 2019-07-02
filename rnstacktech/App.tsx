5b3ed6ba99d09bb75df48b7911198a5e0d7af6fe
Samuel Silva <samuelsilvawb@gmail.com>
2019-07-02 21:54:10 +0100

    feat(rnstacktech): Add Header component
    - Added generic header component.
    - Integrated header at App.js

--- rnstacktech/App.tsx
+++ rnstacktech/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Header } from './src/components';
+
 import { store } from './src/state/store'
 
 export default function App() {
   return (
     <Provider store={ store }>
       <View style={styles.container}>
-        <Text style={styles.text}>StackTech</Text>
+        <Header>
+          <Text>Header</Text>
+        </Header>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>StackTech</Text>
+        </View>
       </View>
     </Provider>
   );
@@ -19,6 +26,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FF6D00',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  textContainer: {
+    flex: 1,
     justifyContent: 'center',
   },
   text: {
