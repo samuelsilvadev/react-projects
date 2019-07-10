0334c25ac59e9c3b0fc8f9792d8608e50cd5c408
Samuel Silva <samuelsilvawb@gmail.com>
2019-07-10 05:21:10 +0100

    chore(rnEmployeeManager): Install redux thunk
    - Installed library.
    - Added middleware on store creation.

--- rnEmployeeManager/src/state/store.ts
+++ rnEmployeeManager/src/state/store.ts
@@ -1,5 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
