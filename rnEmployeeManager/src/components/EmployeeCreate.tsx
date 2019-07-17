8ca9bce6f192b0cf77d27bc2656f7f7376f5f044
Samuel Silva <samuelsilvawb@gmail.com>
2019-07-17 06:03:14 +0100

    feat(rnEmployeeManager): Add EmployeeCreate route
    - Added new route and enabled navigation between them.
    - Added simple component to render new route.

---
+++ rnEmployeeManager/src/components/EmployeeCreate.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text  } from 'react-native';
+
+class EmployeeCreate extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>EmployeeCreate Component</Text>
+			</View>
+		);
+	}
+}
+
+export default EmployeeCreate;
--- rnEmployeeManager/src/components/Routes.tsx
+++ rnEmployeeManager/src/components/Routes.tsx
@@ -1,22 +1,32 @@
 import * as React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 
 import LoginForm from './LoginForm';
 import EmployeeList from './EmployeeList';
+import EmployeeCreate from './EmployeeCreate';
 
 export const Routes = () => (
 	<Router>
 		<Scene key="root" hideNavBar>
 			<Scene key="auth">
-				<Scene key="login" component={LoginForm} title="Please login" initial />
+				<Scene
+					key="login"
+					component={LoginForm}
+					title="Please login"
+					initial />
 			</Scene>
 			<Scene key="main">
 				<Scene
 					rightTitle="Add"
-					onRight={ () => console.log('clicked on right side') }
+					onRight={ () => Actions.employeeCreate() }
 					key="employeeList"
 					component={EmployeeList}
-					title="Employees" />
+					title="Employees"
+					initial />
+				<Scene
+					key="employeeCreate"
+					component={EmployeeCreate}
+					title="Create Employee" />
 			</Scene>
 		</Scene>
 	</Router>
