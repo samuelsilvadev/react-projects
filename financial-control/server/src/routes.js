e9eb516dc23d06401afdd6832e9cb130216591b0
Samuel Silva <samuelsilvawb@gmail.com>
2019-07-28 11:30:26 +0100

    feat(financial-control-server): Add routes to new features
    - Added route to signin/signup and validate your token.

--- financial-control/server/src/routes.js
+++ financial-control/server/src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const BillingCycleController = require('./controllers/billingCycleController');
+const AuthService = require('./services/authService');
+const authMiddleware = require('./middlewares/auth');
 
 function buildRoutes(server) {
-	const router = express.Router();
+	const protectedRouter = express.Router();
+	const unProtectedRouter = express.Router();
 
-	server.use('/api', router);
+	server.use('/api', protectedRouter);
+	protectedRouter.use(authMiddleware);
 
-	BillingCycleController.register(router, '/billing-cycle');
+	BillingCycleController.register(protectedRouter, '/billing-cycle');
+
+	server.use('/o-api', unProtectedRouter);
+	unProtectedRouter.post('/sign-in', AuthService.signIn);
+	unProtectedRouter.post('/sign-up', AuthService.signUp);
+	unProtectedRouter.post('/validate-token', AuthService.validateToken);
 }
 
 module.exports = buildRoutes;
