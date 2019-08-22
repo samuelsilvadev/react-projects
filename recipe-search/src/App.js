77774b81f0e86ba0cce4d24966271358f052f967
Samuel Silva <samuelsilvawb@gmail.com>
2019-08-22 19:03:40 +0100

    feat(recipe-search): Integrate form and recipe list on home
    - Added handler to make the search.
    - Added RecipeList with search result.

--- recipe-search/src/App.js
+++ recipe-search/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Form from './components/Form';
+import RecipesList from './components/RecipesList';
+
+import { recipe } from './API/recipe';
+
 import './App.css';
 
 function App() {
+	const [recipes, setRecipes] = useState([]);
+
+	const handleOnSubmitRecipeSearch = async (event) => {
+		event.preventDefault();
+		const recipeName = event.target.elements.recipeName.value;
+		if (recipeName) {
+			const data = await recipe.get({ searchTerm: recipeName });
+			setRecipes(data.recipes);
+		}
+	}
+
 	return (
-		<div className="App">
+		<div>
+			<header className="header">
+				<h1>Recipe Repository</h1>
+			</header>
+			<main className="main">
+				<Form onSubmit={handleOnSubmitRecipeSearch} />
+				<RecipesList recipes={recipes} />
+			</main>
 		</div>
 	);
 }
