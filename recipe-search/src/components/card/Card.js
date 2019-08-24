4d4f1c752c0d9809624d8ed13314a065b73930e9
Samuel Silva <samuelsilvawb@gmail.com>
2019-08-24 05:43:40 +0100

    refactor(recipe-search): Add support to link on card
    - Added `link` property on Card component.

--- recipe-search/src/components/card/Card.js
+++ recipe-search/src/components/card/Card.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './Card.css';
 
-export const Card = ({ imageSrc, title, description, subtitle, action }) => (
+export const Card = ({ imageSrc, title, description, subtitle, action, link }) => (
 	<div className="card">
 		{imageSrc && <img className="card__image" src={imageSrc} alt={title} />}
 		{title && <h2 className="card__title">{title}</h2>}
 		{subtitle && <p className="card__subtitle">{subtitle}</p>}
 		{description && <p className="card__description">{description}</p>}
 		{action && <button type="button" className="card__action" onClick={action.onClick}>{action.text}</button>}
+		{link && <Link to={link.to} className="card__action card__action--link">{link.text}</Link>}
 	</div>
 );
 
@@ -21,6 +23,10 @@ Card.propTypes = {
 	action: PropTypes.shape({
 		text: PropTypes.string,
 		onClick: PropTypes.func
+	}),
+	link: PropTypes.shape({
+		text: PropTypes.string,
+		to: PropTypes.any
 	})
 };
 
