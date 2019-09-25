4759ec6973e8ea7684bb572b018fed2e5bf9e321
Samuel Silva <samuelsilvawb@gmail.com>
2019-09-25 07:14:21 +0100

    test(addisonglobal-fe-challenge): Add tests to `Events` component
    - Added tests to `Events`;

---
+++ addisonglobal-fe-challenge/src/common/components/events/__tests__/Events.spec.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ConnectedEvents, { Events } from '../Events';
+import { renderWithRedux } from '../../../test-utils/renderWithRedux';
+
+import eventsMock from './events.mock.json';
+
+jest.mock('../../../store/actions/eventsActions', () => ({
+	fetchEvents: () => ({ type: 'GET_EVENTS' })
+}));
+
+describe('<Events />', () => {
+	describe('Unconnected Events Component', () => {
+		it('should call `fetchEvents` after components mount', () => {
+			const props = {
+				fetchEvents: jest.fn()
+			};
+
+			render(<Events {...props} />);
+
+			expect(props.fetchEvents).toHaveBeenCalledTimes(1);
+		});
+
+		it('should render a list of events correctly', () => {
+			const props = {
+				fetchEvents: () => {},
+				events: [
+					{
+						id: 1,
+						name: 'Rick and Morty',
+						markets: [
+							{
+								id: 1,
+								name: 'first trip to mars'
+							},
+							{
+								id: 2,
+								name: 'first robot'
+							}
+						]
+					}
+				]
+			};
+
+			const { container, queryByText } = render(<Events {...props} />);
+
+			expect(queryByText('Rick and Morty')).toBeInTheDocument();
+			expect(queryByText('first trip to mars')).toBeVisible();
+			expect(queryByText('first robot')).toBeVisible();
+			expect(container.firstChild).toMatchSnapshot();
+		});
+	});
+
+	describe('Connected Events', () => {
+		it('should render correctly getting data from store', () => {
+			const initialState = eventsMock;
+
+			const { container, queryByText } = renderWithRedux({
+				initialState
+			})(<ConnectedEvents />);
+
+			expect(queryByText('Real Madrid vs Barcelona')).toBeInTheDocument();
+			expect(queryByText('Team to Win')).toBeVisible();
+			expect(container.firstChild).toMatchSnapshot();
+		});
+	});
+});
