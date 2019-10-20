d7b6a46405846a955a3abc140ec4ab5529da2f22
Samuel Silva <samuelsilvawb@gmail.com>
2019-10-20 06:02:01 +0100

    chore(agrotis-fe-challenge): Configure font-size to use rem as default size unit
    - Added css `font-size` on `html` to be 62.5% from the initial value of the browser.

--- agrotis-fe-challenge/src/common/Global.style.js
+++ agrotis-fe-challenge/src/common/Global.style.js
@@ -7,7 +7,12 @@ export const GlobalStyle = createGlobalStyle`
 		box-sizing: border-box;
 	}
 
+	html {
+		font-size: 62.5%;
+	}
+
 	body {
 		font-family: sans-serif;
+		font-size: 1.6rem;
 	}
 `;
--- agrotis-fe-challenge/src/common/components/header/Header.js
+++ agrotis-fe-challenge/src/common/components/header/Header.js
@@ -3,19 +3,19 @@ import styled from 'styled-components';
 
 import agrotisBrand from '../../images/agrotis.png';
 
-export const HEADER_HEIGHT = '70px';
+export const HEADER_HEIGHT = '7rem';
 
 const StyledHeader = styled.header`
 	align-items: center;
 	display: flex;
-	border-bottom: 1px solid #000;
+	border-bottom: 0.1rem solid #000;
 	height: ${HEADER_HEIGHT};
 	width: 100%;
 `;
 
 const Img = styled.img`
-	margin-left: 50px;
-	width: 120px;
+	margin-left: 5rem;
+	width: 12rem;
 `;
 
 function Header() {
