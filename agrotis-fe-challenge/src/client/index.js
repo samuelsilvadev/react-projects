34151eec1f28cb3204abdc1f91c3983ecf6f17fb
Samuel Silva <samuelsilvawb@gmail.com>
2019-10-31 05:08:01 +0100

    chore(agrotis-fe-challenge): Add reactime
    - Installed dependency.
    - Enabled tool on client render.

--- agrotis-fe-challenge/src/client/index.js
+++ agrotis-fe-challenge/src/client/index.js
@@ -1,14 +1,19 @@
+import reactime from 'reactime';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { hydrate } from 'react-dom';
 import App from '../common/App';
 
+const rootContainer = document.getElementById('root');
+
 hydrate(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
-	document.getElementById('root')
+	rootContainer
 );
+
+reactime(rootContainer);
 
 if (module.hot) {
 	module.hot.accept();
