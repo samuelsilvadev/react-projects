e94d46d94ddd2d9c26628d5903e1499bc41198a1
Samuel Silva <samuelsilvawb@gmail.com>
2019-11-22 09:08:01 +0100

    chore(pokedex-from-mindera): Start project
    - Installed next.
    - Added editorconfig.

---
+++ pokedex-from-mindera/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+	<div>
+		<Head>
+			<title>Pokedex</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	</div>
+)
+
+export default Home
