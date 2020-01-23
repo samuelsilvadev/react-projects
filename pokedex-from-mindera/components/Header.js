ac9070dce77f731226ec84b0ebb602752f54176b
Samuel Silva <samuelsilvawb@gmail.com>
2020-01-23 10:43:00 +0100

    feat(pokedex-from-mindera): Add selected router on menu
    - Added logic to keep navigation item selected when the route match.

--- pokedex-from-mindera/components/Header.js
+++ pokedex-from-mindera/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import css from 'styled-jsx/css';
 
 import SearchInput from './SearchInput';
@@ -35,112 +36,131 @@ const {
 	}
 `;
 
-const Header = () => (
-	<>
-		<style jsx>
-			{`
-				.header {
-					box-shadow: 0 0 2px 0 var(--light-sky-blue);
-					border-top: 5px solid var(--seaweed-green);
-					background-color: var(--white);
-					height: 55px;
-				}
+const navigation = [
+	{
+		route: '/',
+		name: 'Home'
+	},
+	{
+		route: '/favorites',
+		name: 'Favorites'
+	}
+];
 
-				nav,
-				ul {
-					display: flex;
-				}
+const Header = () => {
+	const router = useRouter();
 
-				nav {
-					align-items: center;
-					height: 100%;
-					justify-content: space-between;
-					padding: 0 20px;
-				}
-
-				li:not(:last-child) {
-					margin-right: 22px;
-				}
-
-				li.active:not(:last-child) {
-					margin-right: 18px;
-				}
-
-				.link {
-					font-family: Lato;
-					font-size: 16px;
-					line-height: normal;
-					letter-spacing: 0.13px;
-					color: var(--metallic-blue);
-					text-decoration: none;
-				}
-
-				li.active .link {
-					background-color: var(--greeny-blue);
-					color: var(--white);
-					padding: 1px 7px 3px;
-				}
-
-				h1 {
-					display: none;
-					font-family: Lato;
-					font-size: 24px;
-					font-weight: bold;
-					line-height: normal;
-					letter-spacing: 0.2px;
-					color: var(--dark-blue-green);
-					margin: 0;
-				}
-
-				@media (min-width: ${mediaValues.md}) {
+	return (
+		<>
+			<style jsx>
+				{`
 					.header {
-						height: 69px;
+						box-shadow: 0 0 2px 0 var(--light-sky-blue);
+						border-top: 5px solid var(--seaweed-green);
+						background-color: var(--white);
+						height: 55px;
 					}
 
-					h1 {
-						display: block;
-						margin-right: 32px;
+					nav,
+					ul {
+						display: flex;
 					}
 
 					nav {
-						padding: 0 40px;
-						justify-content: flex-start;
+						align-items: center;
+						height: 100%;
+						justify-content: space-between;
+						padding: 0 20px;
 					}
-				}
-			`}
-		</style>
-		{svgStyles}
-		{svgPokeballStyles}
-		{searchInputStyles}
-		<header className='header'>
-			<nav>
-				<Pokeball
-					className={pokeballIconClassName}
-					width='19px'
-					height='18px'
-				/>
-				<h1>Pokedex</h1>
-				<ul>
-					<li className='active'>
-						<Link href='/'>
-							<a className='link'>Explorer</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/favorites'>
-							<a className='link'>Favorites</a>
-						</Link>
-					</li>
-				</ul>
-				<Search
-					className={searchIconClassName}
-					width='19px'
-					height='19px'
-				/>
-				<SearchInput className={searchInputClassName} />
-			</nav>
-		</header>
-	</>
-);
+
+					li:not(:last-child) {
+						margin-right: 22px;
+					}
+
+					li.active:not(:last-child) {
+						margin-right: 18px;
+					}
+
+					.link {
+						font-family: Lato;
+						font-size: 16px;
+						line-height: normal;
+						letter-spacing: 0.13px;
+						color: var(--metallic-blue);
+						text-decoration: none;
+					}
+
+					li.active .link {
+						background-color: var(--greeny-blue);
+						color: var(--white);
+						padding: 1px 7px 3px;
+					}
+
+					h1 {
+						display: none;
+						font-family: Lato;
+						font-size: 24px;
+						font-weight: bold;
+						line-height: normal;
+						letter-spacing: 0.2px;
+						color: var(--dark-blue-green);
+						margin: 0;
+					}
+
+					@media (min-width: ${mediaValues.md}) {
+						.header {
+							height: 69px;
+						}
+
+						h1 {
+							display: block;
+							margin-right: 32px;
+						}
+
+						nav {
+							padding: 0 40px;
+							justify-content: flex-start;
+						}
+					}
+				`}
+			</style>
+			{svgStyles}
+			{svgPokeballStyles}
+			{searchInputStyles}
+			<header className='header'>
+				<nav>
+					<Pokeball
+						className={pokeballIconClassName}
+						width='19px'
+						height='18px'
+					/>
+					<h1>Pokedex</h1>
+					<ul>
+						{navigation.map(({ name, route }) => {
+							return (
+								<li
+									key={name}
+									className={
+										route === router.route ? 'active' : ''
+									}
+								>
+									<Link href={route}>
+										<a className='link'>{name}</a>
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+					<Search
+						className={searchIconClassName}
+						width='19px'
+						height='19px'
+					/>
+					<SearchInput className={searchInputClassName} />
+				</nav>
+			</header>
+		</>
+	);
+};
 
 export default Header;
