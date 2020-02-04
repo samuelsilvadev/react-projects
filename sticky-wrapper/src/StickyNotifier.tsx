b1fb044429abad64a427c93de222e634299838af
Samuel Silva <samuelsilvawb@gmail.com>
2020-02-04 10:09:00 +0100

    feat(sticky-wrapper): Add notifier events on context

--- sticky-wrapper/src/StickyNotifier.tsx
+++ sticky-wrapper/src/StickyNotifier.tsx
@@ -1,13 +1,32 @@
-import React, { FC, createElement, Fragment, useRef, useEffect, createContext, useState, useContext } from 'react';
+import React, {
+    FC,
+    createElement,
+    Fragment,
+    useRef,
+    useEffect,
+    createContext,
+    useState,
+    useContext,
+    ReactType,
+} from 'react';
 
 import './StickyNotifier.css';
+
+type TargetType = HTMLElement | undefined | null;
 
 interface IStickyContextProps {
     rootRef: HTMLElement | null;
     setRootRef: (value: any) => any;
+    events: IAvailableEvents;
+    setEvents: (events: IAvailableEvents) => void;
 }
 
-interface IProps {
+interface IAvailableEvents {
+    onSticky?: (target: TargetType) => void;
+    onUnSticky?: (target: TargetType) => void;
+}
+
+interface IProps extends IAvailableEvents {
     as: string;
     className?: string;
 }
@@ -16,14 +35,22 @@ interface IStickyNotifierProps {
     as: string;
     className?: string;
     rootRef?: HTMLElement;
+    events?: IAvailableEvents;
 }
+
+const STICKY_ELEMENT_CLASSNAME = 'sticky';
 
 const StickyContext = createContext<Partial<IStickyContextProps>>({});
 
 const StickyContextProvider: FC = ({ children }) => {
     const [rootRef, setRootRef] = useState(null);
+    const [events, setEvents] = useState<IAvailableEvents>({});
 
-    return <StickyContext.Provider value={{ rootRef, setRootRef }}>{children}</StickyContext.Provider>;
+    return (
+        <StickyContext.Provider value={{ rootRef, setRootRef, events, setEvents }}>
+            {children}
+        </StickyContext.Provider>
+    );
 };
 
 const useStickyContext = () => {
@@ -36,24 +63,59 @@ const useStickyContext = () => {
     return context;
 };
 
-const StickyRoot: FC<IProps> = ({ as, ...remainingProps }) => {
-    return (
-        <StickyContextProvider>
-            <StickyContext.Consumer>{({ setRootRef }) => createElement(as, { ref: setRootRef, ...remainingProps })}</StickyContext.Consumer>
-        </StickyContextProvider>
-    );
+const withStickyContextProvider = (Component: ReactType) => {
+    return function WithStickyContextProvider(props: IProps) {
+        return (
+            <StickyContextProvider>
+                <Component {...props} />
+            </StickyContextProvider>
+        );
+    };
 };
+
+const StickyRootWithStickyContextProvider: FC<IProps> = ({
+    as,
+    onSticky,
+    onUnSticky,
+    ...remainingProps
+}) => {
+    const { setEvents, setRootRef } = useContext(StickyContext);
+
+    useEffect(() => {
+        const events: IAvailableEvents = {};
+
+        if (typeof onSticky === 'function') {
+            events.onSticky = onSticky;
+        }
+
+        if (typeof onUnSticky === 'function') {
+            events.onUnSticky = onUnSticky;
+        }
+
+        setEvents && setEvents(events);
+    }, [onSticky, onUnSticky, setEvents]);
+
+    return createElement(as, {
+        ref: setRootRef,
+        ...remainingProps,
+    });
+};
+
+const StickyRoot: FC<IProps> = withStickyContextProvider(StickyRootWithStickyContextProvider);
 
 const StickyWrapper: FC<IProps> = ({ as, className = '', ...remainingProps }) => {
     return createElement(as, { className: `sticky__wrapper ${className}`, ...remainingProps });
 };
 
 const StickyNotifier: FC<IStickyNotifierProps> = ({ as, className, ...remainingProps }) => {
-    const { rootRef } = useStickyContext();
+    const {
+        rootRef,
+        events: { onSticky = undefined, onUnSticky = undefined } = {},
+    } = useStickyContext();
 
     const topSentinel = useRef<HTMLDivElement>(null);
     const bottomSentinel = useRef<HTMLDivElement>(null);
-    const stickyRef = useRef<HTMLElement>(null);
+    const [stickyRefHeight, setStickyRefHeight] = useState<number>(0);
 
     useEffect(() => {
         if (!topSentinel.current || !bottomSentinel.current || !rootRef) {
@@ -62,38 +124,50 @@ const StickyNotifier: FC<IStickyNotifierProps> = ({ as, className, ...remainingP
 
         const topSentinelObserver = new IntersectionObserver(
             ([entry]) => {
-                const { rootBounds, boundingClientRect } = entry;
+                const { rootBounds, boundingClientRect, target } = entry;
+                const $stickyElement = target.parentElement?.querySelector(
+                    `.${STICKY_ELEMENT_CLASSNAME}`,
+                ) as HTMLElement;
 
                 if (boundingClientRect && rootBounds) {
                     if (boundingClientRect.bottom < rootBounds.top) {
-                        console.log('sticky activated');
+                        onSticky && onSticky($stickyElement);
                     }
 
-                    if (boundingClientRect.bottom >= rootBounds.top && boundingClientRect.bottom < rootBounds.bottom) {
-                        console.log('sticky deactivated');
+                    if (
+                        boundingClientRect.bottom >= rootBounds.top &&
+                        boundingClientRect.bottom < rootBounds.bottom
+                    ) {
+                        onUnSticky && onUnSticky($stickyElement);
                     }
                 }
             },
             {
                 threshold: [0],
-                // root: rootRef
+                root: rootRef,
             },
         );
 
         const bottomSentinelObserver = new IntersectionObserver(
             ([entry]) => {
-                const { boundingClientRect, rootBounds, intersectionRatio } = entry;
+                const { boundingClientRect, rootBounds, intersectionRatio, target } = entry;
+                const $stickyElement = target.parentElement?.querySelector(
+                    `.${STICKY_ELEMENT_CLASSNAME}`,
+                ) as HTMLElement;
 
                 if (!boundingClientRect || !rootBounds) {
                     return;
                 }
 
                 if (boundingClientRect.bottom > rootBounds.top && intersectionRatio === 1) {
-                    console.log('sticky activated by bottom sentinel');
+                    onSticky && onSticky($stickyElement);
                 }
 
-                if (boundingClientRect.top < rootBounds.top && boundingClientRect.bottom < rootBounds.bottom) {
-                    console.log('sticky deactivated by bottom sentinel');
+                if (
+                    boundingClientRect.top < rootBounds.top &&
+                    boundingClientRect.bottom < rootBounds.bottom
+                ) {
+                    onUnSticky && onUnSticky($stickyElement);
                 }
             },
             { threshold: [1], root: rootRef },
@@ -106,13 +180,27 @@ const StickyNotifier: FC<IStickyNotifierProps> = ({ as, className, ...remainingP
             topSentinelObserver.disconnect();
             bottomSentinelObserver.disconnect();
         };
-    }, [rootRef]);
+    }, [onSticky, onUnSticky, rootRef]);
 
     return (
         <Fragment>
             <div ref={topSentinel} className="sticky__top__sentinel"></div>
-            {createElement(as, { ref: stickyRef, className: `sticky ${className}`, ...remainingProps })}
-            <div ref={bottomSentinel} className="sticky__bottom__sentinel"></div>
+            {createElement(as, {
+                ref: (node: HTMLElement) => node && setStickyRefHeight(node.offsetHeight),
+                className: `sticky ${className}`,
+                ...remainingProps,
+            })}
+            <div
+                ref={bottomSentinel}
+                className="sticky__bottom__sentinel"
+                style={
+                    stickyRefHeight
+                        ? {
+                              height: `${stickyRefHeight}px`,
+                          }
+                        : undefined
+                }
+            ></div>
         </Fragment>
     );
 };
