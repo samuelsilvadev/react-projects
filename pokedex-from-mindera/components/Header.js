a9f8a9aa0463fb7b980f7acf262957997c56da6f
Samuel Silva <samuelsilvawb@gmail.com>
2020-02-10 21:12:00 +0100

    feat: Update components to use css modules

--- pokedex-from-mindera/components/Header.js
+++ pokedex-from-mindera/components/Header.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import css from 'styled-jsx/css';
 
 import SearchInput from './SearchInput';
 import { Pokeball, Search } from '../icons';
 
-import { mediaValues } from '../styles';
-
-const { className: searchIconClassName, styles: svgStyles } = css.resolve`
-	display: block;
-
-	@media (min-width: ${mediaValues.md}) {
-		display: none;
-	}
-`;
-
-const {
-	className: pokeballIconClassName,
-	styles: svgPokeballStyles
-} = css.resolve`
-	@media (min-width: ${mediaValues.md}) {
-		margin-right: 8px;
-	}
-`;
-
-const {
-	className: searchInputClassName,
-	styles: searchInputStyles
-} = css.resolve`
-	display: none !important;
-
-	@media (min-width: ${mediaValues.md}) {
-		display: flex !important;
-	}
-`;
+import styles from './Header.module.css';
 
 const navigation = [
 	{
@@ -52,85 +23,10 @@ const Header = () => {
 
 	return (
 		<>
-			<style jsx>
-				{`
-					.header {
-						box-shadow: 0 0 2px 0 var(--light-sky-blue);
-						border-top: 5px solid var(--seaweed-green);
-						background-color: var(--white);
-						height: 55px;
-					}
-
-					nav,
-					ul {
-						display: flex;
-					}
-
-					nav {
-						align-items: center;
-						height: 100%;
-						justify-content: space-between;
-						padding: 0 20px;
-					}
-
-					li:not(:last-child) {
-						margin-right: 22px;
-					}
-
-					li.active:not(:last-child) {
-						margin-right: 18px;
-					}
-
-					.link {
-						font-family: Lato;
-						font-size: 16px;
-						line-height: normal;
-						letter-spacing: 0.13px;
-						color: var(--metallic-blue);
-						text-decoration: none;
-					}
-
-					li.active .link {
-						background-color: var(--greeny-blue);
-						color: var(--white);
-						padding: 1px 7px 3px;
-					}
-
-					h1 {
-						display: none;
-						font-family: Lato;
-						font-size: 24px;
-						font-weight: bold;
-						line-height: normal;
-						letter-spacing: 0.2px;
-						color: var(--dark-blue-green);
-						margin: 0;
-					}
-
-					@media (min-width: ${mediaValues.md}) {
-						.header {
-							height: 69px;
-						}
-
-						h1 {
-							display: block;
-							margin-right: 32px;
-						}
-
-						nav {
-							padding: 0 40px;
-							justify-content: flex-start;
-						}
-					}
-				`}
-			</style>
-			{svgStyles}
-			{svgPokeballStyles}
-			{searchInputStyles}
-			<header className='header'>
+			<header className={styles.header}>
 				<nav>
 					<Pokeball
-						className={pokeballIconClassName}
+						className={styles.pokeballIcon}
 						width='19px'
 						height='18px'
 					/>
@@ -141,22 +37,24 @@ const Header = () => {
 								<li
 									key={name}
 									className={
-										route === router.route ? 'active' : ''
+										route === router.route
+											? styles.active
+											: ''
 									}
 								>
 									<Link href={route}>
-										<a className='link'>{name}</a>
+										<a className={styles.link}>{name}</a>
 									</Link>
 								</li>
 							);
 						})}
 					</ul>
 					<Search
-						className={searchIconClassName}
+						className={styles.searchIcon}
 						width='19px'
 						height='19px'
 					/>
-					<SearchInput className={searchInputClassName} />
+					<SearchInput className={styles.searchInput} />
 				</nav>
 			</header>
 		</>
--- pokedex-from-mindera/components/SearchInput.js
+++ pokedex-from-mindera/components/SearchInput.js
@@ -2,49 +2,18 @@ import React from 'react';
 
 import { Search } from '../icons';
 
+import styles from './SearchInput.module.css';
+
 const SearchInput = ({ className = '' }) => {
 	return (
-		<>
-			<style jsx>
-				{`
-					.search {
-						align-items: center;
-						background-color: var(--light-sky-blue);
-						border-radius: 18px;
-						display: flex;
-						margin-left: auto;
-						padding-left: 12px;
-						height: 27px;
-						width: 300px;
-					}
-
-					.search__input {
-						background: none;
-						border: none;
-						padding-left: 10px;
-						font-family: Lato;
-						font-size: 16px;
-						font-weight: 300;
-						line-height: normal;
-						letter-spacing: 0.13px;
-						color: var(--dark-grey-blue);
-						flex-grow: 1;
-					}
-
-					.search__input::placeholder {
-						color: var(--dark-grey-blue);
-					}
-				`}
-			</style>
-			<div className={`search ${className}`}>
-				<Search width='19px' height='19px' />
-				<input
-					className='search__input'
-					type='text'
-					placeholder='Search by name or number'
-				/>
-			</div>
-		</>
+		<div className={`${styles.search} ${className}`}>
+			<Search width='19px' height='19px' />
+			<input
+				className={styles.search__input}
+				type='text'
+				placeholder='Search by name or number'
+			/>
+		</div>
 	);
 };
 
